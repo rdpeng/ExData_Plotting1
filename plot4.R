6f6f985c776a50ca10f16bd2aeed8a1316ab548b
Antonia Rowlinson <b.a.rowlinson@uva.nl>
2014-05-06 21:14:47 +0200

    Plot 4

---
+++ plot4.R
@@ -0,0 +1,35 @@
+
+# Read the data into R
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";") 
+
+# Strip down to only the data on 1/2/2007 and 2/2/2007
+data <- data[which(data$Date=="1/2/2007" | data$Date=="2/2/2007"),]
+
+# save the plot
+png(filename="plot4.png",width=480,height=480,units="px")
+
+# create 2x2 plot
+attach(data)
+par(mfrow=c(2,2))
+
+# extract the dates and times and put them into plottable format
+x <- as.POSIXct(strptime(paste(data$Date,data$Time,sep=" "), format="%d/%m/%Y %H:%M:%S"))
+
+# extract y axis numbers
+y <- as.numeric(as.character(data$Global_active_power))
+y1 <- as.numeric(as.character(data$Sub_metering_1))
+y2 <- as.numeric(as.character(data$Sub_metering_2))
+y3 <- as.numeric(as.character(data$Sub_metering_3))
+z <- as.numeric(as.character(data$Voltage))
+a <- as.numeric(as.character(data$Global_reactive_power))
+
+# create line plots
+plot(x,y,type="l",xlab="",ylab="Global Active Power")
+plot(x,z,type="l",xlab="datetime",ylab="Voltage")
+plot(x,y1,type="l",col="black",xlab="",ylab="Energy sub metering")
+lines(x,y2,col="red")
+lines(x,y3,col="blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd=3, col=c("black","red","blue"))
+plot(x,a,type="l",xlab="datetime",ylab="Global_reactive_power")
+
+dev.off()
