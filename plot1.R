6b28bd76782b694bce268f1d9b98c804191c3de7
Linghuan Zeng <Linghuan@Linghuans-MacBook-Pro.local>
2014-05-10 15:24:18 -0700

    add my assignment1 files

---
+++ plot1.R
@@ -0,0 +1,6 @@
+png("plot1_png",width=480,height=480)
+data<-read.table("household_power_consumption.txt", skip=66637,nrow=2880,head=FALSE,sep=";",col.names=c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+data$DateTime<-paste(data[,1], data[,2])
+data$datetime<-as.POSIXct(strptime(data[,10], "%d/%m/%Y %H:%M:%S"))
+hist(data[,3],main="Global Active Power",xlab="Global Active Power(kilowatts)",col="Red")
+dev.off()
