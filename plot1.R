e20446571bf0c408ced3180f8bb861c58a599e89
Nels Thompson <nelsthompson@yahoo.com>
2014-05-11 19:24:34 -0400

    homework #1

--- plot1.R
+++ plot1.R
@@ -4,3 +4,6 @@ p2 <- pwr[as.Date(pwr$Date, format = "%d/%m/%Y") == "2007-02-02",]
 p <- rbind(p1, p2)
 dt <- paste(p$Date, p$Time)
 dt <- strptime(dt, format = "%d/%m/%Y %H:%M:%S")
+png(file = "plot1.png", width = 480, height = 480)
+hist(as.numeric(p[[3]]), col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
