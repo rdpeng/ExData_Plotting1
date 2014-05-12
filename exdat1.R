4ea602226610cf8bb339a9e782895fc2f72fe6e9
Nels Thompson <nelsthompson@yahoo.com>
2014-05-12 13:50:44 -0400

    adding plot1.png

--- exdat1.R
+++
@@ -1,6 +0,0 @@
-pwr <- read.csv("household_power_consumption.txt", sep=";")
-p1 <- pwr[as.Date(pwr$Date, format = "%d/%m/%Y") == "2007-02-01",]
-p2 <- pwr[as.Date(pwr$Date, format = "%d/%m/%Y") == "2007-02-02",]
-p <- rbind(p1, p2)
-dt <- paste(p$Date, p$Time)
-dt <- strptime(dt, format = "%d/%m/%Y %H:%M:%S")
