bf2610810df23946a7dd8f86f056b2c51244e632
Anshu Gupta <gupta.anshu3@gmail.com>
2014-06-07 09:51:16 +0530

    new

---
+++ Desktop/Exploratory_Data_Analysis/plot1.R
@@ -0,0 +1,7 @@
+#Read table
+power <- read.table("household_power_consumption.txt",header = TRUE,sep = ";")
+power$Date <-as.Date(power$Date, format='%d/%m/%Y')
+powerDate <- power[power$Date %in%  as.Date(c('2007-02-01','2007-02-02')), ]
+png('plot1.png')
+hist(as.numeric(as.character(powerDate[,3])),xlab="Global Active Power(Kilowatts))",col = "red",main = "Global Active Power")
+dev.off()
