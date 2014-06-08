e3abaa4ef0ff6e07f028a9cc06949db124fc24fe
AdrianBadica <badicaadrian1@yahoo.com>
2014-06-08 19:12:42 +0300

    Create plot1.R

---
+++ Project-1/plot1.R
@@ -0,0 +1,5 @@
+data <- read.table("household_power_consumption.txt", sep=";", header=TRUE)
+wdata <- subset(data, Date %in% c("1/2/2007","2/2/2007"))
+png("plot2.png", width=480, height=480, units="px")
+hist(as.numeric(as.character(wdata$Global_active_power)), col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
