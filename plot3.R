126c97677a6693ac51b4b38b7711446833de0902
Subrata Mukherjee <subroto08820@gmail.com>
2014-07-11 18:59:30 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,32 +1,15 @@
-# Exploratory Data Analysis - Assignment 2 - Q. #3
-# Ron Mashrouteh May 23, 2014
+PowerData <- read.csv("household_power_consumption.csv",header=T)
 
-# Load ggplot2 library
-require(ggplot2)
+x <- as.Date(PowerData$Date, format="%d/%m/%Y")
+datetime1 <- paste(x, PowerData$Time)
+datetime <- strptime(datetime1, format="%Y-%m-%d %H:%M:%S")
+PowerData$datetime <- datetime
 
-# Loading provided datasets - loading from local machine
-NEI <- readRDS("~/Exploratory_Data_Analysis/Assignment_2/summarySCC_PM25.rds")
-SCC <- readRDS("~/Exploratory_Data_Analysis/Assignment_2/Source_Classification_Code.rds")
+with(PowerData, plot(datetime, Sub_metering_1,xlab="",ylab = "Energy sub metering",type="n"))
 
-# Sampling
-NEI_sampling <- NEI[sample(nrow(NEI), size=5000, replace=F), ]
+with(PowerData, lines(datetime, Sub_metering_1))
+with(PowerData, lines(datetime, Sub_metering_2, col="red"))
+with(PowerData, lines(datetime, Sub_metering_3, col="blue"))
 
-# Baltimore City, Maryland == fips
-MD <- subset(NEI, fips == 24510)
-MD$year <- factor(MD$year, levels=c('1999', '2002', '2005', '2008'))
+legend("topright", cex=.8, lwd=1, col = c("black","red","blue"), legend = c("Sub_Metering_1","Sub_Metering_2","Sub_Metering_3"))
 
-# Of the four types of sources indicated by the type (point, nonpoint, onroad, nonroad) variable,
-# which of these four sources have seen decreases in emissions from 1999–2008 for Baltimore City?
-# Which have seen increases in emissions from 1999–2008?
-# Use the ggplot2 plotting system to make a plot answer this question.
-
-# Generate the graph in the same directory as the source code
-png(filename='~/Exploratory_Data_Analysis/Assignment_2/plot3.png', width=800, height=500, units='px')
-
-ggplot(data=MD, aes(x=year, y=log(Emissions))) + facet_grid(. ~ type) + guides(fill=F) +
-    geom_boxplot(aes(fill=type)) + stat_boxplot(geom ='errorbar') +
-    ylab(expression(paste('Log', ' of PM'[2.5], ' Emissions'))) + xlab('Year') +
-    ggtitle('Emissions per Type in Baltimore City, Maryland') +
-    geom_jitter(alpha=0.10)
-
-dev.off()
