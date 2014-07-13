d80265f15d4a5cdc2f9a8496324e29ade53cc79f
Xiaodan Zhang <zhang349@illinois.edu>
2014-07-13 00:11:11 -0500

    Added plot 3.

---
+++ plot3.R
@@ -0,0 +1,17 @@
+source("load_data.R")
+dim(newData) # 2880   10
+attach(newData)
+png(filename="plot3.png", 
+    width=480, height=480,
+    units="px", bg="transparent")
+plot(DateTime, Sub_metering_1, 
+     type="l",
+     col="black",
+     xlab="", ylab="Energy sub metering")
+lines(DateTime, Sub_metering_2, col="red")
+lines(DateTime, Sub_metering_3, col="blue")
+legend("topright", 
+       col=c("black", "red", "blue"),
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lwd=1)
+dev.off()
