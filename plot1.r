92cfe7474a5cb53d19b4b66a96d57b57b202a716
chucareer <chucareer@users.noreply.github.com>
2014-07-15 12:35:49 -0700

    Create plot1.r

---
+++ plot1.r
@@ -0,0 +1,7 @@
+##Plot 1
+
+powerData <- readRDS("powerData.rds")
+png(file = "plot1.png", width = 480, height = 480)
+hist(powerData$Global_active_power, col = "red", main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)")
+dev.off()
