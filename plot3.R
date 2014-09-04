fc288679414caf2ce65dcbfa1ad0603a897d42e5
ajr27 <arcenis.rojas@gmail.com>
2014-09-04 19:13:59 -0400

    Files for assignment1

---
+++ plot3.R
@@ -0,0 +1,15 @@
+hhpc <- read.table("household_power_consumption.txt", skip=66637, nrows=2880, sep=";",
+                   stringsAsFactors=FALSE, na.strings="?")
+headings <- unlist(strsplit(readLines("household_power_consumption.txt", 1), ";"))
+colnames(hhpc) <- headings
+rm(headings)
+hhpc$DateTime <- as.POSIXct(paste(hhpc$Date, hhpc$Time), tz="US/Pacific",
+                            format="%d/%m/%Y %H:%M:%S")
+png(file="plot3.png", width=480, height=480)
+plot(hhpc$DateTime, hhpc$Sub_metering_1, type="l", xlab="",
+     ylab="Energy sub metering")
+lines(hhpc$DateTime, hhpc$Sub_metering_2, col="red")
+lines(hhpc$DateTime, hhpc$Sub_metering_3, col="blue")
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lty=1, col=c("black", "red", "blue"))
+dev.off()
