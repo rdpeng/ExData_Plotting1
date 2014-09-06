de6e3e0cbab1d349ffde5d4a481e9c3236dee1cb
James Sweeney <jpsweeney@gmail.com>
2014-09-05 21:56:41 -0400

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,25 +1,20 @@
-myfile <- "~/Downloads/household_power_consumption.txt"
+myfile <- "/Users/jamessweeney2/Desktop/Coursera/household_power_consumption.txt"
 con <- read.table(myfile, sep = ";", header = TRUE)
-a <- con$Date
-b <- as.character(a)
-c <- as.Date(b, "%d/%m/%Y")
+a <- as.character(con$Date)
+c <- as.Date(a, "%d/%m/%Y")
 c <- con$Date
-days <- con[con$Date == "2/2/2007",]
-days2 <- con[con$Date == "1/2/2007",]
-alldays <- rbind(days2, days)
-dt <- paste(alldays$Date, alldays$Time)
+days <- con[con$Date == "1/2/2007" | con$Date == "2/2/2007",]
+dt <- paste(days$Date, days$Time)
 dtf <- dmy_hms(dt)
 newdata <- cbind(dtf, alldays[,3:9])
-png(file = "/Users/jamessweeney/Desktop/plot3.png")
-par(mar = c(4,4,2,1), ps = 12)
+png(file = "/Users/jamessweeney2/Desktop/Coursera/plot3.png")
 with(newdata, plot(dtf, as.numeric(as.character(Sub_metering_1)),
-        type = "l", col = 81, ylab = "Energy sub metering", 
-        xlab = ""))
+                   type = "l", col = 81, ylab = "Energy sub metering", 
+                   xlab = ""))
 with(newdata, lines(dtf, as.numeric(as.character(Sub_metering_2))
-        ,col = "red"))
+                    ,col = "red"))
 with(newdata, lines(dtf, as.numeric(as.character(Sub_metering_3))
-        ,col = "blue"))
+                    ,col = "blue"))
 legend("topright", col = c("gray", "red", "blue"), lty = 1, 
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")) 
-
 dev.off()
