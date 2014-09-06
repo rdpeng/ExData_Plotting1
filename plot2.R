e5fbf22cf036b5eac0911a40a72d7ad3dbc42380
James Sweeney <jpsweeney@gmail.com>
2014-09-05 21:56:20 -0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,15 +1,13 @@
-myfile <- "~/Downloads/household_power_consumption.txt"
+myfile <- "/Users/jamessweeney2/Desktop/Coursera/household_power_consumption.txt"
 con <- read.table(myfile, sep = ";", header = TRUE)
-a <- con$Date
-b <- as.character(a)
-c <- as.Date(b, "%d/%m/%Y")
+a <- as.character(con$Date)
+c <- as.Date(a, "%d/%m/%Y")
 c <- con$Date
-days <- con[con$Date == "2/2/2007",]
-days2 <- con[con$Date == "1/2/2007",]
-alldays <- rbind(days2, days)
-dt <- paste(alldays$Date, alldays$Time)
+days <- con[con$Date == "1/2/2007" | con$Date == "2/2/2007",]
+dt <- paste(days$Date, days$Time)
 dtf <- dmy_hms(dt)
 newdata <- cbind(dtf, alldays[,3:9])
-png(file = "/Users/jamessweeney/Desktop/plot2.png")
-with(newdata, plot(dtf, as.numeric(as.character(Global_active_power)), type = "l", ylab = "Global Active Power (kilowatts)", xlab = ""))
+png(file = "/Users/jamessweeney2/Desktop/Coursera/plot2.png")
+with(newdata, plot(dtf, as.numeric(as.character(Global_active_power)), type = "l", 
+                   ylab = "Global Active Power (kilowatts)", xlab = ""))
 dev.off()
