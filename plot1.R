ed1de0aed57fc301325a15b42a36cff6bd42a6e9
James Sweeney <jpsweeney@gmail.com>
2014-09-05 21:55:55 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,17 +1,14 @@
-myfile <- "~/Downloads/household_power_consumption.txt"
+myfile <- "/Users/jamessweeney2/Desktop/Coursera/household_power_consumption.txt"
 con <- read.table(myfile, sep = ";", header = TRUE)
-a <- con$Date
-b <- as.character(a)
-c <- as.Date(b, "%d/%m/%Y")
+a <- as.character(con$Date)
+c <- as.Date(a, "%d/%m/%Y")
 c <- con$Date
-days <- con[con$Date == "2/2/2007",]
-days2 <- con[con$Date == "1/2/2007",]
-alldays <- rbind(days2, days)
-dt <- paste(alldays$Date, alldays$Time)
+days <- con[con$Date == "1/2/2007" | con$Date == "2/2/2007",]
+dt <- paste(days$Date, days$Time)
 dtf <- dmy_hms(dt)
 newdata <- cbind(dtf, alldays[,3:9])
-png(file = "/Users/jamessweeney/Desktop/plot1.png")
+png(file = "/Users/jamessweeney2/Desktop/Coursera/plot1.png")
 hist(as.numeric(as.character(newdata$Global_active_power)), col = "red", 
-     main = "Global Active Power", xlab = "Global Active Power (kilowatts)
-     ")
+        main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
 dev.off()
+ 
