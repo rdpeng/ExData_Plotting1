5bd861e24844e2a6d06c6518f39ec9c370606a0f
MartinFSSr <martin.smith@acm.org>
2014-10-11 11:14:37 -0400

    Added code to download and upzip project data file
    Previously, all plotx() functions assumed manual download and unzip
    prior to running R code.

--- plot1.R
+++ plot1.R
@@ -1,31 +1,36 @@
 # plot1() function:  creates Exporatory Data Analysis Project 1 plot1.png file
 
-# Prior to running this or the other plotx() functions, download Zipped project data file from
-# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
-# into working directory. UnZip the file, which will create a .txt file in a subdirectory
-# immediately below the working directory.
 
 #Usage: 
-# plot1()         -- generates plot1 in plot1.png file in working directory
-#                 Starts from unZipped (.txt) project data file assumed to be in 
-#                 subdirectory "exdata_data_household_power_consumption" of working directory
+# plot1()         -- generates plot1 in plot1.png file in working directory. 
+#                   First downloads project data file from:
+#                   https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+#                   Then unzips .txt file to temp file and does read.table to produce big data frame "df."
+#                   Next subsets df to produce small data frame "dfx", and generates plot.
 # dfx <- plot1()   -- same as above, but returns dataframe named "dfx" which can be used in subsequent 
-#                 runs of the plotx() functions to save the initial read.table and subsetting step.
-# plot1(dfx)       -- if a plotx() function has been run with results assigned to dfx, the current
-#                 run will pick use that previously created data frame and run faster
+#                  runs of the plotx() functions to save the initial download, read.table and subsetting steps.
+# plot1(dfx)       -- if any plotx() function has been run with results assigned to dfx, the current
+#                  run will use that previously created data frame and run 100x faster
 
 
 plot1 <- function(dfx) {
+  
 
 if (missing(dfx) || !is.data.frame(dfx)) {
-  fname = "exdata_data_household_power_consumption//household_power_consumption.txt"
-  df = read.table(file=fname, header=T, sep=";", na.strings="?", nrows = 0, 
-                  colClasses=c(rep("character",2), rep("numeric",7)))
-
+  
+  temp <- tempfile()
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+  df <- read.table(unz(temp, "household_power_consumption.txt"),header=T, sep=";", na.strings="?", 
+                   nrows = 0, colClasses=c(rep("character",2), rep("numeric",7)) )
+  unlink(temp)
+  
 # Subset the data to keep just two days of interest for this analysis,
 # and add POSIXlt date variable created from two original Date and Time character variables.
   
 dfx = df[df$Date %in% c("1/2/2007","2/2/2007"),]
+
+rm(df)
+
 dfx$DateTime <- strptime(paste(dfx[,1], dfx[,2]), format="%d/%m/%Y %T")
 }
 
--- plot2.R
+++ plot2.R
@@ -1,38 +1,39 @@
 # plot2() function:  creates Exporatory Data Analysis Project 1 plot2.png file
 
-# Prior to running this or the other plotx() functions, download Zipped project data file from
-# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
-# into working directory. UnZip the file, which will create a .txt file in a subdirectory
-# immediately below the working directory.
-
 #Usage: 
-# plot2()                   -- generates plot2 in plot2.png file in working
-#                           directory. Starts from unZipped (.txt) project data file assumed 
-#                           to be in subdirectory "exdata_data_household_power_consumption" 
-#                           of working directory
-# dfx <- plot2()            -- same as  above, but returns dataframe named "dfx" which can be
-#                           used in subsequent runs of the plotx() functions to save the 
-#                           initial read.table and subsetting steps. 
-# plot2(dfx)                 -- if a plotx() function has been run with results assigned to dfx, 
-#                           the current run will use that previously created data frame and run faster 
-# plot2(dfx, color="black")  -- allows #specification of plot line color.  Default is "black"
+# plot2()         -- generates plot1 in plot1.png file in working directory. 
+#                   First downloads project data file from:
+#                   https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+#                   Then unzips .txt file to temp file and does read.table to produce big data frame "df."
+#                   Next subsets df to produce small data frame "dfx", and generates plot.
+# dfx <- plot2()   -- same as above, but returns dataframe named "dfx" which can be used in subsequent 
+#                  runs of the plotx() functions to save the initial download, read.table and subsetting steps.
+# plot2(dfx)       -- if any plotx() function has been run with results assigned to dfx, the current
+#                  run will use that previously created data frame and run 100x faster
 
 
 
-plot2 <- function(df, color="black") {
+
+plot2 <- function(dfx) {
   
-  if (missing(df) || !is.data.frame(df)) {
-    fname = "exdata_data_household_power_consumption//household_power_consumption.txt"
-    df = read.table(file=fname, header=T, sep=";", na.strings="?", nrows = 0, 
-                    colClasses=c(rep("character",2), rep("numeric",7)))
+  if (missing(dfx) || !is.data.frame(dfx)) {
+
+    temp <- tempfile()
+    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+    df <- read.table(unz(temp, "household_power_consumption.txt"),header=T, sep=";", na.strings="?", 
+                     nrows = 0, colClasses=c(rep("character",2), rep("numeric",7)) )
+    unlink(temp)
 
  
 # Subset the big data frame to keep just the 2 days of interest, and
 # create new DateTime column of class "POSIXlt" from original Date and Time character cols
 
 
-dfx = df[df$Date %in% c("1/2/2007","2/2/2007"),]
-dfx$DateTime <- strptime(paste(dfx[,1], dfx[,2]), format="%d/%m/%Y %T")
+  dfx = df[df$Date %in% c("1/2/2007","2/2/2007"),]
+
+  rm(df)
+
+  dfx$DateTime <- strptime(paste(dfx[,1], dfx[,2]), format="%d/%m/%Y %T")
 
   }
 
@@ -41,7 +42,7 @@ dfx$DateTime <- strptime(paste(dfx[,1], dfx[,2]), format="%d/%m/%Y %T")
 
 if (file.exists("plot2.png")) file.remove("plot2.png")
   png(filename="plot2.png")
-  with(dfx, plot(DateTime, Global_active_power, type="l", col=color, xlab="",
+  with(dfx, plot(DateTime, Global_active_power, type="l", xlab="",
                  ylab="Global Active Power (kilowatts)"))
   dev.off()
 
