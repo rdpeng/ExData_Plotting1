867e67c369bebe01cb313b0d913e4e9800c17df3
Frank Murphy Hernandez <murphy@ciencias.unam.mx>
2014-11-09 09:32:24 -0600

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -2,9 +2,6 @@ source('loaddata.R')
 png(filename = "plot1.png",
 width = 480, height = 480,
 units = "px", bg = "transparent")
-hist(data$Global_active_power,
-col = "red",
-main = "Global Active Power",
-xlab = "Global Active Power (kilowatts)",
-breaks = 12, ylim = c(0, 1200))
+hist(data$Global_active_power, main="Global Active Power",
+xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 dev.off()
