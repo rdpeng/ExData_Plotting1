6dacdf2bae6d3355507d2677d88fed835e678eaf
Nels Thompson <nelsthompson@yahoo.com>
2014-11-09 17:19:55 -0500

    path update

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-pwr <- read.csv("household_power_consumption.txt", sep=";")
+pwr <- read.csv("../household_power_consumption.txt", sep=";")
 p1 <- pwr[as.Date(pwr$Date, format = "%d/%m/%Y") == "2007-02-01",]
 p2 <- pwr[as.Date(pwr$Date, format = "%d/%m/%Y") == "2007-02-02",]
 p <- rbind(p1, p2)
