1ff5858139439f15be638f4afb195bd2337e113c
Maria <marylevchenko@gmail.com>
2014-11-09 08:58:05 +0300

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -6,7 +6,9 @@ e$Global_active_power <- as.numeric(e$Global_active_power)
 e$DateTime <- as.POSIXct(paste(e$Date, e$Time))
 
 png("plot3.png", width = 480, height = 480)
-plot(e$DateTime, e$Sub_metering_1, type= "l", lwd=1, col='red')
-lines(e$DateTime, e$Sub_metering_2, type= "l", lwd=1,col='blue')
-lines(e$DateTime, e$Sub_metering_3, type= "l", lwd=1)
+plot(e$DateTime, e$Sub_metering_1, type= "l", lwd=1, col='black')
+lines(e$DateTime, e$Sub_metering_2, type= "l", lwd=1,col='red')
+lines(e$DateTime, e$Sub_metering_3, type= "l", lwd=1, col='blue)
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, bty="n", col=c("black", "red", "blue"))
+
 dev.off()
