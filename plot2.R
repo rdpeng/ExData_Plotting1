07f107d657a608a4ec5cf58e50ed3b12512149ef
Maria <marylevchenko@gmail.com>
2014-11-09 08:47:16 +0300

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -3,4 +3,7 @@ date <- as.Date(dataset$Date, "%d/%m/%Y")
 dataset$Date <- date
 e <- subset(dataset, dataset$Date=='2007-02-01' | dataset$Date=='2007-02-02')
 e$Global_active_power <- as.numeric(e$Global_active_power)
-plot(e$Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
+e$DateTime <- as.POSIXct(paste(e$Date, e$Time))
+png("plot2.png", width=480, height= 480)
+plot(e$DateTime, e$Global_active_power, type= "l", lwd=1, ylab= "Global Active Power (kilowatts)", xlab="")
+dev.off()
