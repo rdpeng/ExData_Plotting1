fa10993a63aeb2d4f24d22f0af686ce8b940e7c4
Maria <marylevchenko@gmail.com>
2014-11-09 08:36:59 +0300

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,6 @@
+dataset <- read.table('household_power_consumption.txt', sep = ';', header=TRUE)
+date <- as.Date(dataset$Date, "%d/%m/%Y")
+dataset$Date <- date
+e <- subset(dataset, dataset$Date=='2007-02-01' | dataset$Date=='2007-02-02')
+e$Global_active_power <- as.numeric(e$Global_active_power)
+hist(e$Global_active_power,xlab="Global Active Power (kilowatts)",col="red",main="Global Active Power")
