2f5e916ba9d6910f72b806f81d2c68cb169503ed
chucareer <chucareer@users.noreply.github.com>
2014-12-03 11:25:46 -0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,6 +1,6 @@
 ##Plot 1
 
-powerData <- readRDS("powerData.rds")
+powerData <- readRDS("powerData.rda")
 png(file = "plot1.png", width = 480, height = 480)
 hist(powerData$Global_active_power, col = "red", main = "Global Active Power", 
      xlab = "Global Active Power (kilowatts)")
