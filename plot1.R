2712ced47d71c1b88b900901483a518873679727
sergioquadros <sergio.quadros@usp.br>
2014-12-04 11:57:07 -0200

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,19 @@
+#exdata-016 sergioquadros 2014-12-04
+library(dplyr)
+library(lubridate)
+library(magrittr)
+library(png)
+
+dat2 <-  tbl_df(read.csv("household_power_consumption.txt",sep=";"))
+dat2$Date <- dmy(dat2$Date)
+dat2 %<>% filter(Date==ymd("2007-02-01")|Date==ymd("2007-02-02"))
+dat2$Time <-hms(dat2$Time)
+class(dat2$Global_active_power) <- "numeric"
+
+png(filename = "plot1.png",
+    width = 480, height = 480, units = "px", bg = "white")
+xx <- (dat2$Global_active_power)/500
+hist(xx,, col = "red")
+title(main = " Global Active Power ", xlab= " Global Active Power(kilowatts) "
+                , ylab= " Frequency ")
+dev.off()
