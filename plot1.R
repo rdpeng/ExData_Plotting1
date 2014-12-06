d67604a11cbcc90d18143a13f5fabd8c1a37bcc5
sergioquadros <sergio.quadros@usp.br>
2014-12-06 00:25:04 -0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -6,15 +6,15 @@ library(lubridate)
 library(magrittr)
 library(png)
 
-dat2 <-  tbl_df(read.csv("household_power_consumption.txt",sep=";"))
+dat2 <-  tbl_df(read.csv("household_power_consumption.txt",sep=";", header=TRUE, as.is=TRUE))
 dat2$Date <- dmy(dat2$Date)
 dat2 %<>% filter(Date==ymd("2007-02-01")|Date==ymd("2007-02-02"))
 dat2$Time <-hms(dat2$Time)
-class(dat2$Global_active_power) <- "numeric"
-xx <- (dat2$Global_active_power)/500.0
+dat2$Global_active_power %<>% as.numeric
 
 png(filename = "plot1.png",
-    width = 480, height = 480, units = "px", bg = "white")
-hist(xx, col = "red", main = " Global Active Power ",
-            xlab= " Global Active Power(kilowatts) ", ylab= " Frequency ")
+    width = 480, height = 480, units = "px", 
+                        bg = "white",res=NA)
+hist(dat2$Global_active_power,col = "red",main ="Global Active Power", 
+          xlab="Global Active Power(kilowatts)", ylab="Frequency")
 dev.off()
