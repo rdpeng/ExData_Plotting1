1956cdaddbef29506c6e259c083bc00454c7647f
mm77548 <meaganmarziani@aol.com>
2014-12-05 23:35:23 -0500

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,26 @@
+##read in the data, and subset it just for the dates needed
+full_data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+data <- full_data[full_data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+## repeat some of the same code from the other plots
+## bring in voltage this time
+datetime <- strptime(paste(data$Date, data$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(data$Global_active_power)
+globalReactivePower <- as.numeric(data$Global_reactive_power)
+voltage <- as.numeric(data$Voltage)
+subMetering1 <- as.numeric(data$Sub_metering_1)
+subMetering2 <- as.numeric(data$Sub_metering_2)
+subMetering3 <- as.numeric(data$Sub_metering_3)
+
+## make all 4 of the plots on the same window pane
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2, 2)) 
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
+
+dev.off()
