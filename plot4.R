94a4e7b33bc8341f118069011f33831a773666ef
IreneZaman <IreneZ473639@gmail.com>
2015-01-11 17:56:04 -0500

    Plot 2-4 Code

---
+++ plot4.R
@@ -0,0 +1,35 @@
+data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+source("load_data.R")
+png(filename = "plot4.png", 
+    width = 480, height = 480,
+    units = "px", bg = "transparent")
+par(mfrow = c(2, 2))
+## Top-left
+plot(DateTime, Global_active_power, 
+     type = "l",
+     xlab = "", ylab = "Global Active Power")
+## Top-right
+plot(DateTime, Voltage,
+     type = "l",
+     xlab = "datetime", ylab = "Voltage")
+## Bottom-left
+plot(DateTime, Sub_metering_1, 
+     type = "l",
+     col = "black",
+     xlab = "", ylab = "Energy sub metering")
+lines(DateTime, Sub_metering_2, col = "red")
+lines(DateTime, Sub_metering_3, col = "blue")
+# Remove the border of legend here.
+legend("topright", 
+       bty = "n",
+       col = c("black", "red", "blue"),
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lwd = 1)
+## Bottom-right
+plot(DateTime, Global_reactive_power, 
+     type = "l",
+     col = "black",
+     xlab = "datetime", ylab = colnames(newData)[4])
+dev.off()
