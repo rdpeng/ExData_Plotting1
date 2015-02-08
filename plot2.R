85d8b12ff74db97f27e7baceee73bbe086f718ad
doncornell <donaldccornell@gmail.com>
2015-02-08 18:32:45 -0500

    Final submissions for February session EDA

--- plot2.R
+++ plot2.R
@@ -1,6 +1,6 @@
 # plot2.R: Project 1, part 2 of Exploratory Data Analysis (Johns Hopkins / Coursera)
 # 
-# updated for January 2015 session of EDA
+# updated for February 2015 session of EDA
 #
 # Reads electrical power consumption data and creates a png file of power over time
 # 
