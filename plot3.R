486d987096b8092be717994a50f03006a10bdfe5
DJMcClellan1966 <DJMcClellan1966@gmail.com>
2015-03-07 12:18:11 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,5 +1,5 @@
 setwd("~/Desktop/coursera")
-rm(list=ls())
+rm(list=ls())## reove files for clarity
 summary("household_power_consumption.txt")
 
 hpc <- read.csv("household_power_consumption.txt",
@@ -7,15 +7,18 @@ hpc <- read.csv("household_power_consumption.txt",
                 dec=".",
                 stringsAsFactors= FALSE,
                 na.strings = "?",
-                colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
-)
+                colClasses=c("character", "character", "numeric", "numeric", "numeric", 
+                "numeric", "numeric", "numeric", "numeric")) ## sets column classes
 summary(hpc)
 
-subSet_hpc <- hpc[hpc$Date %in% c("1/2/2007","2/2/2007") ,]
+subSet_hpc <- hpc[hpc$Date %in% c("1/2/2007","2/2/2007") ,]## subsets dates used in plots
 summary(subSet_hpc)
 
 datetime <- strptime(paste(subSet_hpc$Date, subSet_hpc$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
-globalActivePower <- as.numeric(subSet_hpc$Global_active_power)
+## converts date and time from character to object
+
+## as.numeric converts data to a more workable form
+GlobalActivePower <- as.numeric(subSet_hpc$Global_active_power)
 subMetering1 <- as.numeric(subSet_hpc$Sub_metering_1)
 subMetering2 <- as.numeric(subSet_hpc$Sub_metering_2)
 subMetering3 <- as.numeric(subSet_hpc$Sub_metering_3)
@@ -25,9 +28,10 @@ plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
 lines(datetime, subMetering2, type="l", col="red")
 lines(datetime, subMetering3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
-dev.off()
+dev.off()## save plot to R
 plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
 lines(datetime, subMetering2, type="l", col="red")
 lines(datetime, subMetering3, type="l", col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
-
+                legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+                , lty=1, lwd=2.5, col=c("black", "red", "blue"))
+##puts plot in device after saving, need to remember to turn off device
