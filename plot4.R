fdc518b71ef2c138c6da7b04f7f666e86799fad1
DJMcClellan1966 <DJMcClellan1966@gmail.com>
2015-03-07 12:24:29 -0500

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -39,7 +39,7 @@ legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=
 
 plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
 
-dev.off()
+dev.off()## saves plot to R
 par(mfrow = c(2, 2))
 
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
@@ -52,4 +52,5 @@ lines(datetime, subMetering3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
 
 plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
+##need to remeber to turn device off after finished using plot
 
