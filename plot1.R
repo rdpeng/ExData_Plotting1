5aa34d6c9487f53e27f103962c254a4e627f5d80
DJMcClellan1966 <DJMcClellan1966@gmail.com>
2015-03-07 08:32:10 -0500

    Create plot1.R
    plot 1 R code

---
+++ plot1.R
@@ -0,0 +1,20 @@
+setwd("~/Desktop/coursera ")
+rm(list=ls())
+
+summary("household_power_consumption.txt")
+hpc <- read.csv("household_power_consumption.txt", 
+                sep=";",
+                dec=".", 
+                stringsAsFactors= FALSE,
+                na.strings = "?",
+                colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
+)
+summary(hpc)
+                subSet_hpc <- hpc[hpc$Date %in% c("1/2/2007", "2/2/2007"),]
+summary(subSet_hpc)
+
+
+png("plot1.png", width=480, height=480)
+plot <- as.numeric(subSet_hpc$Global_active_power)
+hist(plot, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
