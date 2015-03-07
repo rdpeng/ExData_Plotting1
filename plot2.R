2cddab490a1ef32a334d6dbb9d6de3b7fafe214b
DJMcClellan1966 <DJMcClellan1966@gmail.com>
2015-03-07 11:31:37 -0500

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,22 @@
+setwd("~/Desktop/coursera")
+rm(list=ls())
+summary("household_power_consumption.txt")
+
+hpc <- read.csv("household_power_consumption.txt",
+                sep=";",
+                dec=".",
+                stringsAsFactors= FALSE,
+                na.strings = "?",
+                colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
+)
+summary(hpc)
+
+subSet_hpc <- hpc[hpc$Date %in% c("1/2/2007","2/2/2007") ,]
+summary(subSet_hpc)
+
+datetime <- strptime(paste(subSet_hpc$Date, subSet_hpc$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower<- as.numeric(subSet_hpc$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
