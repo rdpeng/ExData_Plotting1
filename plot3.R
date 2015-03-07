e80673c867682bf606fccfed767cecd7a767d5ed
Doug <DJMcClellan1966@gmail.com>
2015-03-07 15:30:58 -0500

    plots
    various plots for course project 1

--- plot3.R
+++ plot3.R
@@ -1,4 +1,4 @@
- setwd("~/Desktop/coursera /ExData_Plotting1")
+setwd("~/Desktop/coursera /ExData_Plotting1")
 rm(list=ls())## remove files for clarity
 summary("household_power_consumption.txt")
 
@@ -7,8 +7,8 @@ hpc <- read.csv("household_power_consumption.txt",
                 dec=".",
                 stringsAsFactors= FALSE,
                 na.strings = "?",
-                colClasses=c("character", "character", "numeric", "numeric", "numeric", 
-                "numeric", "numeric", "numeric", "numeric")) ## sets column classes
+                colClasses=c("character", "character", "numeric", "numeric", "numeric",
+                             "numeric", "numeric", "numeric", "numeric")) ## sets column classes
 summary(hpc)
 
 subSet_hpc <- hpc[hpc$Date %in% c("1/2/2007","2/2/2007") ,]## subsets dates used in plots
@@ -32,6 +32,5 @@ dev.off()## save plot to R
 plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
 lines(datetime, subMetering2, type="l", col="red")
 lines(datetime, subMetering3, type="l", col="blue")
-                legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
-                , lty=1, lwd=2.5, col=c("black", "red", "blue"))
-##puts plot in device after saving, need to remember to turn off device
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+       , lty=1, lwd=2.5, col=c("black", "red", "blue"))
