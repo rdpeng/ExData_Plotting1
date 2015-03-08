5114017b4510bc35d2154be950e09785532da060
sbarzucco2000 <mpacilio2005@hotmail.it>
2015-03-08 12:01:45 +0100

    Create load_data.R

---
+++ load_data.R
@@ -0,0 +1,18 @@
+## This file is for loading the large dataset.
+filename <- "./data/household_power_consumption.txt"
+data <- read.table(filename,
+                   header = TRUE,
+                   sep = ";",
+                   colClasses = c("character", "character", rep("numeric",7)),
+                   na = "?")
+dim(data) # 2075259 9
+attach(data)
+## We only need data of 2 days.
+subset <- Date == "1/2/2007" | Date == "2/2/2007"
+newData <- data[subset, ]
+attach(newData)
+x <- paste(Date, Time)
+newData$DateTime <- strptime(x, "%d/%m/%Y %H:%M:%S")
+rownames(newData) <- 1:nrow(newData)
+dim(newData) # 2880   10
+attach(newData)
