d6262a54c33145d5515ed1317bdd4b14c72fb333
Purushottam <pursh2002@yahoo.co.in>
2015-03-09 00:02:39 +0300

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,13 @@
+household <- read.csv("E:/Directory.R/explorotory data analysis/household.txt", sep=";")
+subset <- household[household$Date %in% c("1/2/2007","2/2/2007") ,]
+datetime <- strptime(paste(subset$Date, subset$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+powerplot <- as.numeric(subset$Global_active_power)
+subMetering1 <- as.numeric(subset$Sub_metering_1)
+subMetering2 <- as.numeric(subset$Sub_metering_2)
+subMetering3 <- as.numeric(subset$Sub_metering_3)
+png("plot3.png", width=480, height=480)
+plot(datetime,subMetering1, type="l", xlab="", ylab="Energy Submetering")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.off()
