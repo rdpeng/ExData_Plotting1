50cc7837204d24f6c40122a9730544dcc7d20239
Owlgo <mark.curley@verizon.net>
2015-04-10 12:59:07 -0700

    project1 4_10_15

--- plot3.R
+++ plot3.R
@@ -2,10 +2,15 @@ plot3<-function(fil="household_power_consumption.txt"){
 
 data<-read.table(fil,sep=";",na.strings="?",header=TRUE)
 #
+# combine day and time
+#
 x<-paste(data$Date, data$Time)
 data$x<-strptime(x,"%d/%m/%Y %H:%M:%S")
 data$Date<-as.Date(data$Date,"%d/%m/%Y")
 data$Date2<-data$Date
+#
+#use only those data required for problem
+#
 prb<-c("01/02/2007","02/02/2007")
 prb<-as.Date(prb,"%d/%m/%Y")
 prb<-as.numeric(as.POSIXct(prb))
@@ -13,6 +18,9 @@ data$Date<-as.numeric(as.POSIXct(data$Date))
 data<-subset(data,data$Date>=prb[1])
 data<-subset(data,data$Date<=prb[2])
 y<-data$Sub_metering_1
+#
+#plot data
+#
 png(file = "Plot3.png", bg = "transparent",width=480,height=480)
 plot(data$x, y, type="l",xlab="", ylab="Energy sub metering")
 with(data, points(data$x, data$Sub_metering_2,type="l", col="red"))
--- plot4.R
+++ plot4.R
@@ -2,16 +2,24 @@ plot4<-function(fil="household_power_consumption.txt"){
 
 data<-read.table(fil,sep=";",na.strings="?",header=TRUE)
 #
+# combine day and time
+#
 x<-paste(data$Date, data$Time)
 data$x<-strptime(x,"%d/%m/%Y %H:%M:%S")
 data$Date<-as.Date(data$Date,"%d/%m/%Y")
 data$Date2<-data$Date
+#
+#use only those data required for problem
+#
 prb<-c("01/02/2007","02/02/2007")
 prb<-as.Date(prb,"%d/%m/%Y")
 prb<-as.numeric(as.POSIXct(prb))
 data$Date<-as.numeric(as.POSIXct(data$Date))
 data<-subset(data,data$Date>=prb[1])
 data<-subset(data,data$Date<=prb[2])
+#
+#plot data
+#
 png(file = "Plot4.png", bg = "transparent",width=480,height=480)
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 with(data,{
