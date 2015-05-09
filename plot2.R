d6f35914a5e578049d7c4d598750ecc25f7059ec
DJMcClellan1966 <DJMcClellan1966@gmail.com>
2015-05-09 12:17:42 -0400

    code for plot 2

---
+++ plot2.R
@@ -0,0 +1,26 @@
+setwd("~/Desktop/coursera /eda") ## set working directory
+rm(list=ls()) ##remove files fr clarity
+
+hpc <- read.csv("household_power_consumption.txt",
+                sep=";",
+                dec=".",
+                stringsAsFactors= FALSE,
+                na.strings = "?",
+                colClasses=c("character", "character", "numeric", "numeric", "numeric", 
+                "numeric", "numeric", "numeric", "numeric")) ## sets column classes
+summary(hpc)
+
+subSet_hpc <- hpc[hpc$Date %in% c("1/2/2007","2/2/2007") ,] ## subsets dates used in plots
+str(subSet_hpc)
+
+datetime <- strptime(paste(subSet_hpc$Date, subSet_hpc$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+##converts character to object class
+
+globalActivePower<- as.numeric(subSet_hpc$Global_active_power) 
+
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()##saves plot to R
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+## places plot in device after saving, remeber to turn device off after done viewing
+
