185928e9f7c0dc2b26bdb6f08fa9934d82e59536
YuanyuanCHN <panyuanyuan.chn@gmail.com>
2015-06-05 20:15:47 +0800

    Update Data Input.R

--- R-Code/Data Input.R
+++ R-Code/Data Input.R
@@ -1,4 +1,8 @@
 This is the data input part.
   #Get full Dataset
   data_full <- read.csv("household_power_consumption.txt",header = T, sep = ";", na.string = "?", nrows = 2075259, check.names = F, stringAsFactor = F, comment.char = "", quote = '\"')
-  
+  #Convert Date
+  data_full$Date <- as.Date(data_full$Date, format = "%d/%m/%y")
+  #Subset Data
+  Data <- subset(data_full, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+  rm(data_full)
