5c0c361a5093ee5c7d532bb589c1192c9b4cc966
YuanyuanCHN <panyuanyuan.chn@gmail.com>
2015-06-05 20:23:20 +0800

    Update plot3.R

--- R-Code/plot3.R
+++ R-Code/plot3.R
@@ -6,3 +6,7 @@ with(Data, {
 })
 legend("topright", col = c("black", "red", "blue"), lty = 1, lwd = 2,
       legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3")) 
+
+## Saves data to file
+dev.copy(png, file = "plot3.png", height = 480, width = 480)
+dev.off()
