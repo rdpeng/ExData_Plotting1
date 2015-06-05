ad4e428d2dcc69b55c887b55908c7bb79d3ae27f
YuanyuanCHN <panyuanyuan.chn@gmail.com>
2015-06-05 20:20:20 +0800

    Create plot3.R

---
+++ R-Code/plot3.R
@@ -0,0 +1,8 @@
+￼## Generating Plot 3
+with(Data, {
+    plot(Sub_metering_1 ~ Datetime, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "") 
+    lines(Sub_metering_2 ~ Datetime, col = 'Red') 
+    lines(Sub_metering_3 ~ Datetime, col = 'Blue')
+})
+legend("topright", col = c("black", "red", "blue"), lty = 1, lwd = 2,
+      legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3")) 
