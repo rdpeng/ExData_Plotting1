11165ee46765006916b96c98aa0efac8a1062487
YuanyuanCHN <panyuanyuan.chn@gmail.com>
2015-06-05 20:21:09 +0800

    Create plot4.R

---
+++ R-Code/plot4.R
@@ -0,0 +1,15 @@
+￼## Generating Plot 4
+par(mfrow = c(2,2), mar = c(4,4,2,1), oma = c(0,0,2,0)) 
+with(Data, {
+  plot(Global_active_power ~ Datetime, type = "l",
+       ylab = "Global Active Power", xlab = "")
+  plot(Voltage ~ Datetime, type = "l", ylab = "Voltage", xlab = "datetime") 
+  plot(Sub_metering_1 ~ Datetime, type = "l", ylab = "Energy sub metering",xlab = "")
+  lines(Sub_metering_2 ~ Datetime, col = 'Red')
+  lines(Sub_metering_3 ~ Datetime, col = 'Blue')
+  legend("topright", col = c("black","red", "blue"), lty = 1, lwd = 2,
+         bty = "n",
+         legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")) 
+  plot(Global_reactive_power ~ Datetime, type = "l",
+       ylab = "Global_rective_power", xlab = "datetime")
+})
