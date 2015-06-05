1b9e4de3590548f235911d12c1f3327a7cf6ba37
YuanyuanCHN <panyuanyuan.chn@gmail.com>
2015-06-05 20:22:52 +0800

    Update plot1.R

--- R-Code/plot1.R
+++ R-Code/plot1.R
@@ -1,3 +1,6 @@
 #Graph 1
 hist(Data$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
 
+## Saves data to file
+dev.copy(png, file = "plot1.png", height = 480, width = 480)
+dev.off()
