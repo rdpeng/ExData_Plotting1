a2a569b37df98ca2d69a4183acee6b7211085390
AndresFrancoPerez <affp99@gmail.com>
2015-06-05 22:37:48 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,9 +1,10 @@
-datos=read.csv('household_power_consumption.txt',header=T, sep=';') # read the textfile which is formatted as a csv
-datos$Datetime = paste(as.character(datos[,1]) , datos[,2])  # merge date and time into a single column (all string variables)
-datos[,1]=as.Date(datos$Date,'%d/%m/%Y') # convert the Date column's datatype from string to date 
-datos= subset(datos, Date == '2007-02-01' | Date == '2007-02-02') #select only the two required days
-datos[,3] = as.numeric(as.character(datos[,3])) # convert the Global Active Power's datatype from text (interpreted as factors, hence the as.character) to numbers
-datos$fecha<-strptime(datos$Datetime, '%d/%m/%Y %H:%M')
-png("plot2.png")
-plot(datos$fecha,datos$Global_active_power,ylab='Global Active Power (kilowatts)',xlab='',type='line')
-dev.off()
+#This creates the Plot2
+datos=read.csv('household_power_consumption.txt',header=T, sep=';') ##This reads the cvs file
+datos$Datetime = paste(as.character(datos[,1]) , datos[,2])  ##Combines Date and Time in 1 column
+datos[,1]=as.Date(datos$Date,'%d/%m/%Y') ##Change the attribute form text to Date
+datos= subset(datos, Date == '2007-02-01' | Date == '2007-02-02') ##Subset for the required dates
+datos[,3] = as.numeric(as.character(datos[,3])) ## Takes the Global Active Power's, and changes it to numbers
+datos$fecha<-strptime(datos$Datetime, '%d/%m/%Y %H:%M') ##Get the right date type
+png("plot2.png") ##Open a new PNG device
+plot(datos$fecha,datos$Global_active_power,ylab='Global Active Power (kilowatts)',xlab='',type='line') ##Create the plt
+dev.off() #Close the device
