0578f3c95f10390b724dd44958d6f008a49c0734
debratee <debratee@umich.edu>
2015-06-07 12:03:27 -0400

    Create plot1.R

---
+++ figure/plot1.R
@@ -0,0 +1,12 @@
+setwd("~/Downloads")
+dat = read.table("household_power_consumption.txt", sep=";", header=TRUE)
+summary(dat)
+head(dat)
+
+as.Date(dat$Date)
+
+newdat = subset(dat, Date=="1/2/2007" | Date=="2/2/2007")
+
+newdat$Global_active_power=as.numeric(newdat$Global_active_power)/500
+hist(newdat$Global_active_power, col="red", main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", breaks=20, ylim=c(0,1200))
