f85b43a432e398fad2e76dc6fd0fcfa6f3d7459e
Anne-Marie Ciza AGHANZE <acamny03@yahoo.fr>
2015-06-08 01:13:09 +0100

    Project1 _ Exploratory Data Analysis
    Exploratory Data Analysis_ poroject1

--- plot3.R
+++ plot3.R
@@ -1,11 +1,3 @@
-  zipFile <- "exdata-data-household_power_consumption.zip"
-  textFile <- "household_power_consumption.txt"
-  colClasses <- c("character", "character", rep("numeric",7))
-  data <- read.csv(unz(zipFile, textFile), header = TRUE, sep = ";", na.strings = "?", colClasses= colClasses)
-  data$DateTime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M")
-  data$Date <- as.Date(data$Date, "%d/%m/%Y")
-  data <- data[data$Date >= as.Date("2007/02/01") & data$Date <= ("2007/02/02"),]
-
 title = ""
 x_axis = ""
 y_axis = "Energy sub metering"
@@ -30,5 +22,5 @@ axis(1,at=c(0,length(data$Date)/2,length(data$Date)),labels=c("Thu","Fri","Sat")
 
 legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
 
-dev.copy(png,"figure/plot3.png",width=480,height=480)
+dev.copy(png,file = "plot3.png")
 dev.off()
