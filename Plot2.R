2520703ae9fdd71d214b22df4913beadf2ad2a25
LSandefur <lindzeyshay@gmail.com>
2015-07-12 16:45:53 -0500

    Create Plot2.R

---
+++ Plot2.R
@@ -0,0 +1,8 @@
+plot2 <- function() {
+       plot(df$timestamp,df$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+       dev.copy(png, file="plot2.png", width=480, height=480)
+       dev.off()
+       cat("plot2.png has been saved in", getwd())
+}
+plot2()
+plot2.png has been saved in /Users/Lindzey/data
