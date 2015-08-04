20ddf7985c2eaa9f75bec2fe0c3f4ceb7a2eecb2
triharder <travis.wethington@gmail.com>
2015-08-03 22:12:47 -0400

    Updated

--- plot2.R
+++ plot2.R
@@ -5,7 +5,7 @@ days <- subset(pwr,pwr$Date >= "2007-02-01" & pwr$Date <= "2007-02-02")
 days$Time <- as.POSIXct(strptime(paste(days$Date, days$Time), "%Y-%m-%d %H:%M:%S"))
 row.names(days) <- NULL
 
-with(days, plot(Time,Global_active_power,type="l", ylab="Global Active Power (kilowatts)"))
+with(days, plot(Time,Global_active_power,type="l", xlab="", ylab="Global Active Power (kilowatts)"))
 
 ## Copy plot to a PNG file
 dev.copy(png, file = "plot2.png", height=480, width=480, units="px")
