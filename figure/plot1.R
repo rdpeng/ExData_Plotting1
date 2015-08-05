b5fc616ba96190ef441e0c967bc8109bf58dbebc
debratee <debratee@umich.edu>
2015-08-04 21:16:57 -0500

    Update plot1.R

--- figure/plot1.R
+++ figure/plot1.R
@@ -10,3 +10,5 @@ newdat = subset(dat, Date=="1/2/2007" | Date=="2/2/2007")
 newdat$Global_active_power=as.numeric(newdat$Global_active_power)/500
 hist(newdat$Global_active_power, col="red", main="Global Active Power", 
      xlab="Global Active Power (kilowatts)", ylab="Frequency", breaks=20, ylim=c(0,1200))
+
+
