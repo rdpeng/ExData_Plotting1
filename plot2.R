5d96df1de113d34cdd7a43b5e80b67f42c1e179d
jesusarielpena <jesusarielpena@gmail.com>
2015-08-06 17:57:38 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -9,7 +9,7 @@ data_full <- read.csv("household_power_consumption.txt", sep=";", na.strings = "
 #Now filter the two days that will be used for the analysis using filter function.
 prueba<- filter(data_full, Date=="1/2/2007"| Date=="2/2/2007")
 
-#convert Dates from chr format to Date format. (too see formar write str(prueba))
+#convert Dates from chr format to Date format. 
 prueba$Date <- as.Date(prueba$Date, format = "%d/%m/%Y") 
 
 # Combine Date and Time variables to create  a new column in dataset named "DateTime" in as.POSIXct format 
