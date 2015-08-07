7867c6f852f1cbe1c97a70528b75fbd85392cfbd
carbanesi <carbanesi@users.noreply.github.com>
2015-08-07 18:32:17 -0300

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,19 @@
+# set working directory (change this to fit your needs)
+setwd('~/Source Code/GitHub/Exploratory-Data-Analysis')
+
+# make sure the plots folder exists
+if (!file.exists('plots')) {
+  dir.create('plots')
+}
+
+# load data
+source('scripts/get_and_clean_data.R')
+
+# open device
+png(filename='plots/plot1.png',width=480,height=480,units='px')
+
+# plot data
+hist(power.consumption$GlobalActivePower,main='Global Active Power',xlab='Global Active Power (kilowatts)',col='red')
+
+# Turn off device
+x<-dev.off()
