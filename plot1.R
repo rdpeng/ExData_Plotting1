867fda7fd2bed4554a3235931e92317f28ce8624
mazinmelb <amanda.belton@gmail.com>
2015-09-12 10:55:56 +1000

    Updated comments
    Improve clarity of code

--- plot1.R
+++ plot1.R
@@ -4,23 +4,16 @@
 
 filename="household_power_consumption.txt"
 
-## start reading from the first date "2007-02-01"
-## this was calculated using
-## nrow(newdata[newdata$Date < "2007-02-01",])
-## skiprows = 66636
-## read the nbr of rows to the last entry for 2007-02-02
-## this was calculated using
-## nrow(newdata[newdata$Date=="2007-02-02"|newdata$Date=="2007-02-01",])
-# nbrrows = 2880
-# newdata <- read.table(filename,sep=";",header=TRUE,na.strings="?",nrows = nbrrows, skip=skiprows,stringsAsFactors=FALSE)
-
+# read in the UCI data and subset to first two days in February
 newdata <- read.table(filename,sep=";",header=TRUE,na.strings="?",stringsAsFactors=FALSE)
 newdata[,"Date"] <- as.Date(newdata[,"Date"],format= "%d/%m/%Y")
 newdata<-newdata[newdata$Date=="2007-02-02"|newdata$Date=="2007-02-01",]
 
+# add a new column of the date in POSIXct format
 newdata<-cbind(as.POSIXct(strptime(paste(newdata[,1],newdata[,2]),format="%Y-%m-%d %H:%M:%S")), newdata)
 names(newdata)[1]<-"ctTime"
 
+# create a graph of global active power, as a png file
 pngname="plot1.png"
 # png(filename=pngname,width=480, height=480)
 Power.name <- "Global Active Power (kilowatts)"
