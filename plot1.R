0ee2de1f239bbcdf7e357ef32f014e9d62f258da
GS <grldsndrs@outlook.com>
2015-09-13 17:32:29 -0400

    Plot2 complete

--- plot1.R
+++ plot1.R
@@ -1,48 +1,114 @@
-Introduction
+# Introduction
+#
+# This assignment uses data from the UC Irvine Machine Learning Repository, a
+# popular repository for machine learning datasets. In particular, we will be
+# using the “Individual household electric power consumption Data Set” which I
+# have made available on the course web site:
+#
+# Dataset: Electric power consumption [20Mb]
+#
+# Description: Measurements of electric power consumption in one household with
+# a one-minute sampling rate over a period of almost 4 years. Different
+# electrical quantities and some sub-metering values are available.
+#
+# The following descriptions of the 9 variables in the dataset are taken from
+# the UCI web site:
+#
+# Date: Date in format dd/mm/yyyy Time: time in format hh:mm:ss
+# Global_active_power: household global minute-averaged active power (in
+# kilowatt) Global_reactive_power: household global minute-averaged reactive
+# power (in kilowatt) Voltage: minute-averaged voltage (in volt)
+# Global_intensity: household global minute-averaged current intensity (in
+# ampere) Sub_metering_1: energy sub-metering No. 1 (in watt-hour of active
+# energy). It corresponds to the kitchen, containing mainly a dishwasher, an
+# oven and a microwave (hot plates are not electric but gas powered).
+# Sub_metering_2: energy sub-metering No. 2 (in watt-hour of active energy). It
+# corresponds to the laundry room, containing a washing-machine, a tumble-drier,
+# a refrigerator and a light. Sub_metering_3: energy sub-metering No. 3 (in
+# watt-hour of active energy). It corresponds to an electric water-heater and an
+# air-conditioner. Loading the data
+#
+# When loading the dataset into R, please consider the following:
+#
+# The dataset has 2,075,259 rows and 9 columns. First calculate a rough estimate
+# of how much memory the dataset will require in memory before reading into R.
+# Make sure your computer has enough memory (most modern computers should be
+# fine).
+#
+# We will only be using data from the dates 2007-02-01 and 2007-02-02. One
+# alternative is to read the data from just those dates rather than reading in
+# the entire dataset and subsetting to those dates.
+#
 
-This assignment uses data from the UC Irvine Machine Learning Repository, a popular repository for machine learning datasets. In particular, we will be using the “Individual household electric power consumption Data Set” which I have made available on the course web site:
 
-  Dataset: Electric power consumption [20Mb]
+# You may find it useful to convert the Date and Time variables to Date/Time
+# classes in R using the strptime() and as.Date() functions.
+#
+# Note that in this dataset missing values are coded as ?.
+library(data.table)
+library(xts)
+library(dplyr)
 
-Description: Measurements of electric power consumption in one household with a one-minute sampling rate over a period of almost 4 years. Different electrical quantities and some sub-metering values are available.
+#Set path to csv
+path2txt <- paste0(getwd(),"/ExData_Plotting1/household_power_consumption.txt")
 
-The following descriptions of the 9 variables in the dataset are taken from the UCI web site:
 
-  Date: Date in format dd/mm/yyyy
-Time: time in format hh:mm:ss
-Global_active_power: household global minute-averaged active power (in kilowatt)
-Global_reactive_power: household global minute-averaged reactive power (in kilowatt)
-Voltage: minute-averaged voltage (in volt)
-Global_intensity: household global minute-averaged current intensity (in ampere)
-Sub_metering_1: energy sub-metering No. 1 (in watt-hour of active energy). It corresponds to the kitchen, containing mainly a dishwasher, an oven and a microwave (hot plates are not electric but gas powered).
-Sub_metering_2: energy sub-metering No. 2 (in watt-hour of active energy). It corresponds to the laundry room, containing a washing-machine, a tumble-drier, a refrigerator and a light.
-Sub_metering_3: energy sub-metering No. 3 (in watt-hour of active energy). It corresponds to an electric water-heater and an air-conditioner.
-Loading the data
+#read in csv as a dataframe
+mydf <- read.csv2(path2txt,
+                  #skip down to the rows of interest          #Grab 2 days of data
+                  skip=grep("^1/2/2007", readLines(path2txt)), nrows=2*24*60,
+                  stringsAsFactors = FALSE,header = FALSE )
 
-When loading the dataset into R, please consider the following:
+#create data frame table
+hpc<-tbl_df(mydf)
 
-  The dataset has 2,075,259 rows and 9 columns. First calculate a rough estimate of how much memory the dataset will require in memory before reading into R. Make sure your computer has enough memory (most modern computers should be fine).
 
-We will only be using data from the dates 2007-02-01 and 2007-02-02. One alternative is to read the data from just those dates rather than reading in the entire dataset and subsetting to those dates.
 
-You may find it useful to convert the Date and Time variables to Date/Time classes in R using the strptime() and as.Date() functions.
+setnames(hpc, old = 1:9, new = strsplit(readLines(path2txt,n=1),";")[[1]])
 
-Note that in this dataset missing values are coded as ?.
+hpcNoDateTime <- select(hpc, 3:9)
 
-Making Plots
+hpcDateTime<- paste( hpc$Date, hpc$Time,sep = " ")
 
-Our overall goal here is simply to examine how household energy usage varies over a 2-day period in February, 2007. Your task is to reconstruct the following plots below, all of which were constructed using the base plotting system.
+hpcDateTime<- strptime( hpcDateTime,"%d/%m/%Y %H:%M:%S")
 
-First you will need to fork and clone the following GitHub repository: https://github.com/rdpeng/ExData_Plotting1
+row.names(hpcNoDateTime) <- hpcDateTime
 
-For each plot you should
+hpcTimeSeries <- hpcNoDateTime
 
-Construct the plot and save it to a PNG file with a width of 480 pixels and a height of 480 pixels.
+hpcTimeSeries<- as.data.frame(sapply(hpcTimeSeries, as.numeric))
 
-Name each of the plot files as plot1.png, plot2.png, etc.
+hpcXTS <- xts(hpcTimeSeries, order.by=hpcDateTime)
 
-Create a separate R code file (plot1.R, plot2.R, etc.) that constructs the corresponding plot, i.e. code in plot1.R constructs the plot1.png plot. Your code file should include code for reading the data so that the plot can be fully reproduced. You must also include the code that creates the PNG file.
-Add the PNG file and R code file to the top-level folder of your git repository (no need for separate sub-folders)
-When you are finished with the assignment, push your git repository to GitHub so that the GitHub version of your repository is up to date. There should be four PNG files and four R code files, a total of eight files in the top-level folder of the repo.
+# Making Plots
+#
+# Our overall goal here is simply to examine how household energy usage varies
+# over a 2-day period in February, 2007. Your task is to reconstruct the
+# following plots below, all of which were constructed using the base plotting
+# system.
+#
+# First you will need to fork and clone the following GitHub repository:
+# https://github.com/rdpeng/ExData_Plotting1
+#
+# For each plot you should
+#
+# Save it to a PNG file
+#
+# Name each of the plot files as plot1.png, plot2.png, etc.
+png(filename = paste0(getwd(),"/ExData_Plotting1/plot1.png"), width = 480, height = 480)
+# with a width of 480 pixels and a height of 480 pixels.
 
-The four plots that you will need to construct are shown below.
+# Construct the plot
+hist( hpcXTS[,1],
+      col = "red",
+      main = "Global Active Power",
+      xlab = "Global Active Power (kilowatts)")
+
+dev.off()
+
+
+
+
+#
+# Create a separate R code file (plot1.R, plot2.R, etc.) that constructs the
+# corresponding plot, i.e. code in plot1.R constructs the plot1.png plot.
