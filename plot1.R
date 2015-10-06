f97b12cad25ce54f9e6b3dc3f06bf1b2d5a849d3
Ji-Youn Sung <jyune15@gmail.com>
2015-10-06 18:30:51 +0900

    updated version
    This is 2nd trial for Course project

--- plot1.R
+++ plot1.R
@@ -1,17 +1,12 @@
 ##setwd("C:/Users/ocsuser/Desktop/Coursera work/EDA/")
-library(dplyr)
 dt <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings ="?") ## read data file
 head(dt)
 str(dt)
-dt2 <- mutate(dt, Date_Time = paste(dt[,1],dt[,2]))  ## add new variables of date and time
-datetime <- strptime(dt2[,10], "%d/%m/%Y %H:%M:%S")  ## make a vector of date and time class changed from dt2$Date_Time 
-dt2 <- cbind(dt2, datetime) ## add a date and time class vector as 'datetime' in dt2
+dt2 <- dt[dt$Date %in% c("1/2/2007", "2/2/2007"),]  ## new data frame of subset during 2007-02-01~02
+dt2$datetime <- strptime(paste(dt2[,1],dt2[,2]), "%d/%m/%Y %H:%M:%S") ## make a vector of date and time class changed from dt2$Date_Time 
 head(dt2)
 str(dt2)
-dt3 <- dt2[(dt2[,11]>="2007-02-01 00:00:00" & dt2[,11]<"2007-02-02 24:00:00"),] ## new data frame of subset during 2007-02-01~02
-head(dt3)
-str(dt3)
 
 png(file="plot1.png", width=480, height=480, units="px")
-with(dt3, hist(Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency"))
+with(dt2, hist(Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency"))
 dev.off()
