07363f7421cc7829f290630851f936c0c35d3a84
horrorkumani <ngominhhuong2005@yahoo.com>
2015-10-11 21:22:00 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-data <- read.table("C:/Users/Thanh/Downloads/exdata-data-household_power_consumption/household_power_consumption.txt", header = TRUE, sep = ";", dec = ".")
+data <- read.table("C:/Users/HP/Documents/GitHub/Huong/household_power_consumption.txt", header = TRUE, sep = ";", dec = ".")
 data$Date <- as.Date(as.character(data$Date), format = "%d/%m/%Y")
 d1<-as.Date("01/02/2007",format="%d/%m/%Y")
 d2<-as.Date("02/02/2007",format="%d/%m/%Y")
