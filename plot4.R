e15887d4f3bd0044e0c28e3f5504a9660e97027b
maxcar <maxcar@users.noreply.github.com>
2015-10-11 15:13:05 +0100

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,47 @@
+
+setwd("~/R/Explanatory Data Analysis/Assignement01/exdata-data-household_power_consumption")
+downloadFileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+downloadFileTarget <- "./Data/household_power_consumption.zip"
+household_power_consumption <- "./Data/household_power_consumption.txt"
+##
+if (!file.exists(household_power_consumption)) {
+  download.file(downloadFileURL, downloadFileTarget, method = "curl")
+  if (!file.exists(downloadFileTarget)) {
+    unzip(downloadFileTarget, overwrite = T, exdir = "./Data")
+  }
+}
+
+## Getting full dataset
+data_full <- read.csv(household_power_consumption, header = T, sep = ';', 
+                      na.strings = "?", nrows = 2075259, check.names = F, 
+                      stringsAsFactors = F, comment.char = "", quote = '\"')
+data_full$Date <- as.Date(data_full$Date, format = "%d/%m/%Y")
+
+## Subsetting the data
+data <- subset(data_full, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
+
+## Converting dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+
+## Generating Plot 4
+
+
+
+par(mfrow = c(2,2), mar = c(4,4,2,1), oma = c(0,0,2,0))
+with(data, {
+  plot(Global_active_power ~ Datetime, type = "l", 
+       ylab = "Global Active Power", xlab = "")
+  plot(Voltage ~ Datetime, type = "l", ylab = "Voltage", xlab = "datetime")
+  plot(Sub_metering_1 ~ Datetime, type = "l", ylab = "Energy sub metering",
+       xlab = "")
+  lines(Sub_metering_2 ~ Datetime, col = 'Red')
+  lines(Sub_metering_3 ~ Datetime, col = 'Blue')
+  legend("topright", col = c("black", "red", "blue"), lty = 1, lwd = 2, 
+         bty = "n",
+         legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(Global_reactive_power ~ Datetime, type = "l", 
+       ylab = "Global_rective_power", xlab = "datetime")
+})
