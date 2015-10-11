e2b7adc55e4ab1276c9c6ce7137d47f1add3ceb0
my-katie <katieinhongkong@homail.com>
2015-10-11 22:19:08 +0800

    Update Plot 1.R

--- Plot 1.R
+++ Plot 1.R
@@ -1,12 +1,12 @@
 ## Read from file and download only data within the required period
-
 dataFile <- "./household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-## Plot 1 histogram
-## Convert  'Global Active Power' to numeric
+## Convert to numeric
 globalActivePower <- as.numeric(subSetData$Global_active_power)
+
+## Plot 1 histogram
 hist(globalActivePower, main="Global Active Power",  
       xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red") 
  
