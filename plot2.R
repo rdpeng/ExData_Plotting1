3c8810c211ba3deaac26a792b23e99d925da917b
patrickchanou <pcasimi@ju.edu>
2015-11-03 13:59:20 -0500

    plot2.R#

--- plot2.R
+++ plot2.R
@@ -1,4 +1,4 @@
-## Create the second plot in the working directory
+### Create the second plot in the working directory
 plot1 <- function() {
         plot(df$timestamp,df$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
         dev.copy(png, file="plot2.png", width=480, height=480)
