ec73b4375bbfc4b4a50c539d51b33c87d9a9a1f2
Cocu23 <c_kukuk@hotmail.com>
2015-11-06 11:57:51 +0100

    Course Project 1

--- plot2.R
+++ plot2.R
@@ -2,10 +2,6 @@
 # Course Project 1
 # 6/11/2015 by Cocu23
 
-# Set Working Directory
-setwd("/Users/christiankukuk/R Workspace/Exploratory Data Analysis")
-getwd()
-
 # Read Data
 powerconsumption <- read.table("household_power_consumption.txt",header = TRUE, sep = ";",na.strings="?")
 
