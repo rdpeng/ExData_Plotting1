b6a9d2399d7dfc8f7bc7a1f542d54e7f65c7574f
Alex <tufnoodle@users.noreply.github.com>
2015-11-06 08:58:04 -0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,20 +1,32 @@
+# Check and install data.table package if req'd
+if(!require("data.table")){
+ install.packages("data.table")   
+}
+
+library(data.table)
+
 # Check to see if the data file is downloaded
 if (!file.exists("./household_power_consumption.txt")) {
     
-    # download the data
+    # Download data
     fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
     fileName <- "household_power_consumption.zip"
     download.file(fileUrl, fileName, method = "curl")
     
-    # unzip file
+    # Unzip file
     unzip(fileName)
-    }
+}
 
-# read data  
-data <- read.table("./household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
+fileName <- "./household_power_consumption.txt"
+
+# Read data
+dat <- fread(input = fileName, na.strings = "?")
+
+# Subset data to only dates 2007-02-01 and 2007-02-02
+# subsetDat <- dat[dat$Date == "01/02/2007" | dat$Date == "02/02/2007", ]
 
 # convert Date variable to Date class
-data$Date <- as.Date(data$Date, "%d/%m/%Y")
+dat$Date <- as.Date(dat$Date, "%d/%m/%Y")
 
 # return subset data
-data[data$Date == "2007/02/01" | data$Date == "2007/02/02", ]
+subsetDat <- dat[dat$Date == "2007/02/01" | dat$Date == "2007/02/02", ]
