467896fe47663f6b189a286bdf2de74da2c202b5
Lilie Kho <liekho@gmail.com>
2015-11-09 00:42:55 +0800

    Revert "project1"
    This reverts commit 89d972147bd58652dbc001a1ec743493f5fbb4ae.

--- plot1.R
+++
@@ -1,9 +0,0 @@
-dataFl <- "./data/household_power_consumption.txt"
-data <- read.table(dataFl, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-
-#str(subSetData)
-gloActivePower <- as.numeric(subSetData$Global_active_power)
-png("plot1.png", width=480, height=480)
-hist(gloActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
-dev.off()
