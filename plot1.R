46c9435e9f3614b346c498cb1c16be2945f6c0fc
LadyRisingmoon <mlaten@gmail.com>
2015-11-08 17:57:32 -0500

    Project 1

---
+++ plot1.R
@@ -0,0 +1,20 @@
+# Plot 1: Bar chart of Global Active Power vs. Frequency for 2007-02-01 and 2007-02-02 from "household_power_consumption.txt" dataset
+setwd("C:/Users/Michelle/dataScience/ExploratoryData")
+
+# Read the text file and handle missing dataset values which are coded as "?" 
+mydata <- read.csv("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?")
+
+# Convert between character representations and objects of class "Date" representing calendar dates
+mydata$Date <- as.Date(mydata$Date, format="%d/%m/%Y")
+
+# Subset dates to analyze:  2007-02-01 and 2007-02-02
+subsetdata <- subset(mydata, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+# Plot 1: Title, x-axis label, y-axis label, and colored in red
+hist(subsetdata$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+# Save Plot 1 to a PNG file named "plot1.png" which has a width of 480 pixels and a height of 480 pixels
+dev.copy(png, file='plot1.png', height=480, width=480)
+hist(subsetdata$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+dev.off()
+
