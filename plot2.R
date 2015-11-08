83acc5b23ee5f17f2bd304e72e102508e1f2c374
Maksym <MaksymUsik@yandex.ru>
2015-11-08 18:09:44 +0200

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -6,7 +6,7 @@ res=res[res$Date<as.Date("2007/02/03"),]
 
 ## Plot2
 
-png("Plot2.png", width=4, height=4, units="in", res=300)
+png("Plot4.png", width=480, height=480, units="px")
 
 plot(1:2880, as.numeric(as.vector(res$Global_active_power)), type = "l", xaxt = "n", xlab="", ylab = "Global Active Power (kilowatts)")
 axis(1, c(0, 1440, 2880), c("Thu", "Fri", "Sat"))
