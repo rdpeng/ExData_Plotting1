172d26805578a2553346d292df0c82db547bac6f
Shailaja Suresh <Shailajasur@gmail.com>
2015-12-11 20:09:45 -0600

    Remove

--- plot.R
+++
@@ -1,5 +0,0 @@
-plot<-function(){
-  mydata = read.table("household_power_consumption.txt",na.strings = "?",sep=";",header=TRUE)
-  mydata$Date<-as.Date(mydata$Date, format = "%d/%m/%Y")
-  data<-subset(mydata,mydata$Date==as.Date(factor("2007-02-01"))|mydata$Date==as.Date(factor("2007-02-02")))
-}
