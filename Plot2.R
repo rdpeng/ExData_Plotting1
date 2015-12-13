401516282366bec87f8689e17f27540767b63c1a
HuiSee <yhui.see@gmail.com>
2015-12-14 00:26:19 +0800

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,5 +1,5 @@
 ## read data ## 
-data <-read.table("F:\\Coursea\\04. Exploratory Data Analysis\\Course Project\\Course Project1\\household_power_consumption.txt", sep=";", header=TRUE)
+data <-read.table("F:\\Course Project1\\household_power_consumption.txt", sep=";", header=TRUE)
 subset_data <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 ## defined axis ##
@@ -7,6 +7,6 @@ datetime <- strptime(paste(subset_data$Date, subset_data$Time), format="%d/%m/%Y
 globalActPower <- as.numeric(subset_data$Global_active_power)
 
 ## plot the graphic ##
-png("F:\\Coursea\\04. Exploratory Data Analysis\\Course Project\\Course Project1\\plot2.png", width=480, height=480)
+png("F:\\Course Project\\Course Project1\\plot2.png", width=480, height=480)
 plot(datetime,globalActPower, type="1", xlab=" ", ylab="Global Active Power(kilowatts)")
 dev.off()
