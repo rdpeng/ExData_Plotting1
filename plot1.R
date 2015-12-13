0d29fbb487ef1371b1a0661b091eab817076e90e
Lilie Kho <liekho@gmail.com>
2015-12-13 23:18:29 +0800

    commit

--- plot1.R
+++ plot1.R
@@ -5,5 +5,5 @@ subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 #str(subSetData)
 gloActivePower <- as.numeric(subSetData$Global_active_power)
 png("plot1.png", width=480, height=480)
-hist(gloActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+hist(gloActivePower, col="red", main="Global Active Power", xlab="Global Active Power(kilowatts)")
 dev.off()
