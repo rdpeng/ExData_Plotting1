5df25f1cc93ade4f147ddb561bb0c060a363ce40
DARC8man <drewmccure@hotmail.com>
2015-12-13 16:53:37 -0600

    Project 1
    file for project 1

---
+++ plot2.R
@@ -0,0 +1,18 @@
+#Plot 2 for Coursea Project
+
+#Data setup
+data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";",na.strings = "?",colClasses = c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+
+data$Time <- paste(data$Date, data$Time,sep = " ")
+
+
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+data$Time <- strptime(data$Time, format = "%d/%m/%Y %H:%M:%S")
+
+dataUse <- subset(data, data$Date == as.Date("2007-02-01") | data$Date == as.Date("2007-02-02"))
+#plot 2
+png("plot2.png", width = 480, height = 480)
+par(mfrow=c(1,1))
+plot(dataUse$Time, dataUse$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+
+dev.off()
