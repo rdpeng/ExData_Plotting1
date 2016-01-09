9355fd114484f2db02f07009dafef4caa6195bee
jonperez <JonPerez@MacBook-Pro-de-Jon.local>
2016-01-09 20:45:52 +0100

    plot3

---
+++ plot3.R
@@ -0,0 +1,7 @@
+plot(dataday$Time, dataday$Sub_metering_1, type="l", ylab="Energy sub metering", cex.lab=0.7,xlab="")
+lines(dataday$Time, dataday$Sub_metering_2, col="red")
+lines(dataday$Time, dataday$Sub_metering_3, col="blue")
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1,1), col=c("black","red", "blue"), cex=0.8)
+locale_original <- Sys.getlocale( category = "LC_TIME" )
+dev.copy(png,file="plot3.png")
+dev.off()
