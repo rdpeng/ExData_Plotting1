8ebcb97358b68af87df6fd1e7250c6c9873637f6
jonperez <JonPerez@MacBook-Pro-de-Jon.local>
2016-01-09 20:16:48 +0100

    plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
 par(mfrow=c(1,1))
-hist(dataday$Global_active_power,breaks=20,col="red",ylim=c(0,1500),main = "Global Active Power")
+hist(dataday$Global_active_power,breaks=20,col="red",ylim=c(0,1500),main = "Global Active Power",xlab="Global Active Power (kilowatts)")
 dev.copy(png,'plot1.png',  width = 480, height = 480)
 dev.off()
