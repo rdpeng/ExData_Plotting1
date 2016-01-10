1fd4d19c8774c0c372582fa2ee43835653b746fc
rishabhkale <rishabhkale@gmail.com>
2016-01-10 12:46:10 -0600

    Added comments
    added description of location of each individual graph

--- plot4.R
+++ plot4.R
@@ -12,20 +12,20 @@ png('plot4.png',width=480,height=480)
 # Setting the plot window to be for 4 graphs 2x2 (column wise)
 par(mfcol=c(2,2))
 
-# Frist
+# Frist - row 1, col 1
 with(new_power_consumption,plot(strptime(paste(Date,Time),"%d/%m/%Y %H:%M:%S"),as.numeric(Global_active_power),type="l",xlab="",ylab="Global Active Power"))
 
-# Second
+# Second - row 2, col 1
 with(new_power_consumption,plot(strptime(paste(Date,Time),"%d/%m/%Y %H:%M:%S"),as.numeric(Sub_metering_1),type="n",xlab="",ylab="Energy sub metering"))
 with(new_power_consumption,lines(strptime(paste(Date,Time),"%d/%m/%Y %H:%M:%S"),as.numeric(Sub_metering_1),type="l",col="black"))
 with(new_power_consumption,lines(strptime(paste(Date,Time),"%d/%m/%Y %H:%M:%S"),as.numeric(Sub_metering_2),type="l",col="red"))
 with(new_power_consumption,lines(strptime(paste(Date,Time),"%d/%m/%Y %H:%M:%S"),as.numeric(Sub_metering_3),type="l",col="blue"))
 legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),lty=c(1,1,1),bty="n",cex=0.5,inset=0)
 
-# Third
+# Third - row 1, col 2
 with(new_power_consumption,plot(strptime(paste(Date,Time),"%d/%m/%Y %H:%M:%S"),as.numeric(Voltage),type="l",xlab="datetime",ylab="Voltage"))
 
-# Fourth
+# Fourth - row 2, col 2
 with(new_power_consumption,plot(strptime(paste(Date,Time),"%d/%m/%Y %H:%M:%S"),as.numeric(Global_reactive_power),type="l",xlab="datetime",ylab="Global_reactive_power"))
 
 dev.off()
