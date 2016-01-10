21743590fa6f2f2f101199e3ee7dbaaabcd5a6c7
euler2718 <euler2718@users.noreply.github.com>
2016-01-10 13:42:37 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,5 +1,6 @@
 #in this, when I tried to subset the data BEFORE reading it in, I kept receiving the following error: Error in plot.window(...) : need finite 'xlim' values.
 #reading the full data in first solved the problem but admittedly I don't know why I had infinite time values????
+
 mydata <- read.table("household_power_consumption.txt",header=TRUE,sep=";",colClasses=c(rep("character",2), rep("numeric",7)),na="?")
 
 #also, I found out I didn't have to change the column names!  So I am keeping this format for the reast of my plots....
@@ -10,9 +11,6 @@ mydata <- subset(mydata, Date %in% as.Date(c("2007-02-01", "2007-02-02"), "%Y-%m
 
 png("plot2.png")
 
-plot(mydata$Time, mydata$Global_active_power,
-     type="l",
-     xlab="",
-     ylab="Global Active Power (kilowatts)")
+plot(mydata$Time, mydata$Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)")
 
 dev.off()
