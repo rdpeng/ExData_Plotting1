93ddb333f40f73afd84f6514f9d5903ccd2c315f
fctap <tap@foundationcenter.org>
2016-01-10 11:52:02 -0500

    Final Version

--- plot4.R
+++ plot4.R
@@ -1,4 +1,6 @@
-household <-read.table( "C:/coursera/datatoolbox/exploreData/household.txt",  sep = ";", fill=TRUE)
+#load in Data
+household <-read.table( "household.txt",  sep = ";", fill=TRUE)
+#rename fields
 names(household)[1]=paste("Date")
 names(household)[2]=paste("Time")
 names(household)[3]=paste("Global_active_power")
@@ -9,18 +11,23 @@ names(household)[7]=paste("Sub_metering_1")
 names(household)[8]=paste("Sub_metering_2")
 names(household)[9]=paste("Sub_metering_3")
 
+#Set the date field to a date
 household[,1]<-as.Date(household[,1],"%d/%m/%Y")
 
+#create logical vector for date range
+houseSet <-household$Date>="2007-02-01"&household$Date <"2007-02-03"
 
+#limit the results to date range
+houseData<-household[houseSet,]
 
- houseSet <-household$Date>="2007-02-01"&household$Date <"2007-02-03"
- 
-h<-cbind(strptime(paste(household$Date,household$Time),"%Y-%m-%d %H:%M:%S"),household) 
-
+#Create datetime field   
+h<-cbind(strptime(paste(houseData$Date,houseData$Time),"%Y-%m-%d %H:%M:%S"),houseData) 
 names(h)[1]=paste("datetime")
 
-
+#set up png file
 png(file= "plot4.png", width=480, height =480)
+
+#create chart
 par(mfrow = c(2,2))
 with(data,{
 plot(h$datetime,as.numeric(as.character(h$Global_active_power)),type="l",ylab="Global Active Power",xlab="")
