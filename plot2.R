c15f2193132d0e6a5e51d44d30c4761e83bfedee
rishabhkale <rishabhkale@gmail.com>
2016-01-10 12:37:45 -0600

    plot2.R file
    Created R file for creating the second graph

---
+++ plot2.R
@@ -0,0 +1,16 @@
+# Read file using read.csv function with delimeter as ';'. 
+power_consumption<-read.csv(file="exdata-data-household_power_consumption/household_power_consumption.txt",sep=";")
+
+# Take the subset of file as per the required date range. Note - data type conversion is done
+# as by default all columns are factors. The data could have been read as per required format but I find this easier
+new_power_consumption<-subset(power_consumption,as.Date(Date,"%d/%m/%Y")>=as.Date("2007-02-01") & as.Date(Date,"%d/%m/%Y")<=as.Date("2007-02-02"))
+
+# Setting the plot window for 1 graph
+par(mfrow=c(1,1))
+
+# Graph 2
+# Specifying 480x480 pixels as per requirement
+png('plot2.png',width=480,height=480)
+with(new_power_consumption,plot(strptime(paste(Date,Time),"%d/%m/%Y %H:%M:%S"),as.numeric(Global_active_power),type="l",xlab="",ylab="Global Active Power (kilowatts)"))
+#dev.copy(png,'plot2.png')
+dev.off()
