e7c9485c402b71411809cbca3bbe8281214a2332
EmmanuelRamos <jemmanuel_harrison@hotmail.com>
2016-01-14 19:42:12 -0600

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -3,7 +3,9 @@ data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec="
 sub <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 rm(data)
 sub$Global_active_power <- as.numeric(sub$Global_active_power)
-png("plot1.png",width = "480",height = "480")
-with(sub,hist(x =subSetData$Global_active_power,xlab = "Global Active Power (kilowatts)",main = "Global Active Power",col="red"))
+png("plot1.png",width = 480,height = 480)
+with(sub,hist(x =sub$Global_active_power,xlab = "Global Active Power (kilowatts)",main = "Global Active Power",col="red"))
 dev.off()
+
+
 
