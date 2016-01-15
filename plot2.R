424801afb6caef933a14859d5beb14bfd6e93c59
EmmanuelRamos <jemmanuel_harrison@hotmail.com>
2016-01-14 19:42:56 -0600

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -3,8 +3,8 @@ data <- read.table(file, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 sub<- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 rm(data)
 
-datime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-globalActive <- as.numeric(subSetData$Global_active_power)
+datime <- strptime(paste(sub$Date, sub$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActive <- as.numeric(sub$Global_active_power)
 png("plot2.png", width=480, height=480)
 plot(datime, globalActive, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
