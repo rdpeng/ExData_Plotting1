f5b73fc48a4deda224757caeef3ddf0036e962a3
RLI0602 <rli@apartnership.com>
2016-02-01 15:11:54 -0500

    Create plot1.R

--- plot1.R
+++ plot1.R
@@ -1,5 +1,5 @@
 ## Getting full dataset
-data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?",
+data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
 nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
 
