b4e5d9dca8221a1720ccaa19cbea542f0b6ad77d
keadz2305 <keadz2305@gmail.com>
2016-02-04 02:39:06 +0800

    Update for plot 4

--- plot4.R
+++ plot4.R
@@ -14,7 +14,7 @@ plot(epctemp2$dt,epctemp2$Voltage,type="l", main=" ",ylab="Voltage",xlab="dateti
 plot(epctemp2$dt,epctemp2$Sub_metering_1,type="l", main=" ",ylab="Energy Sub Metering",xlab=" ")
 lines(epctemp2$dt,epctemp2$Sub_metering_2,type="l", col="red")
 lines(epctemp2$dt,epctemp2$Sub_metering_3,type="l", col="blue")
-legend('topright', c("Sub_metering_1","Sub_metering_2","Sub_metering_3") , lty=0.5, col=c("black","red","blue"),cex=0.3, xjust=1, yjust=1,x.intersp=0.2, y.intersp=0.1,inset = 0)
+legend('topright', c("Sub_metering_1","Sub_metering_2","Sub_metering_3") , lty=1, col=c("black","red","blue"),cex=0.5, xjust=1, yjust=1,x.intersp=0.2, y.intersp=0.1,inset = 0,bty='n')
        
 plot(epctemp2$dt,epctemp2$Global_reactive_power,type="l", main=" ",ylab="Global_reactive_power",xlab="datetime ")
 
