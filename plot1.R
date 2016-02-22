4dfee65b216bade2443b187c7ada3c6bdab93cb1
Felix L. Griffin <felixlgriffin@msn.com>
2016-02-21 19:28:06 -0600

    Added files via upload

---
+++ plot1.R
@@ -0,0 +1,7 @@
+plot1 <- function() {
+  hist(df$Global_active_power, main = paste("Global Active Power"), col="red", xlab="Global Active Power (kilowatts)")
+  dev.copy(png, file="plot1.png", width=480, height=480)
+  dev.off()
+  cat("Plot1.png has been saved in /Users/Felix/Documents/Exploratory Data Analysis", getwd())
+}
+plot1()
