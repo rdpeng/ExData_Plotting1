2953643ed34318456b59ebdf1fa5663d234d6a14
lingrenyun <lingyun.y11@gmail.com>
2016-03-13 22:01:15 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,15 +1,10 @@
-#Read the file
-data<-read.table("household_power_consumption.txt",header = TRUE,sep = ";")
-
-#Format the Date
-data$Date <- as.Date(data$Date,"%d/%m/%Y")
+#Read in the file
+hpc<-read.table("household_power_consumption.txt", header=TRUE,sep=";",na.strings="?")
 
 #Subset the data with Date of "2007-02-01" & "2007-02-02"
-newdata<-data[which(data$Date=="2007-02-01" | data$Date=="2007-02-02"),]
+hpc_sub<-subset(hpc,Date == '2/2/2007'|Date == '1/2/2007')
 
-#Create Plot1 and output
-newdata$Global_active_power<-as.numeric(as.character(newdata$Global_active_power))
-Global_active_power<-newdata$Global_active_power
-png("plot1.png",480,480)
-hist(Global_active_power,main="Global Active Power", col="red", xlab="Global Active Power(kilowatts)")
+#Create Plot1 and Output PNG File
+png("plot1.png",width=480, height=480)
+hist(hpc_sub$Global_reactive_power,xlab="Global Active Power (kilowatts)",col="red",main="Global Active Power")
 dev.off()
