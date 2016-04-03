13d93a364e7cb105ddc2a6a809ab2ca96c270d0c
Sohom Majumdar <sohom.majumdar@inmobi.com>
2016-04-03 20:53:11 +0530

    Added Plot 3

---
+++ Plot3.R
@@ -0,0 +1,19 @@
+# This will plot the frequency of Global Active power in the dataset
+plot.subm <- function(data_file, png_file) {
+  data <- read_data(data_file)
+  png(png_file)
+  data$datetime <- as.POSIXct(paste(data$Date, data$Time))
+  data$maxSubMeter <- apply(data[,7:9], 1, max)
+  plot(data$datetime, data$maxSubMeter, 
+       main = "Energy Sub Metering", type = "n",
+       ylab = "Energy sub-metering", xlab = "Day of the week")
+  lines(data$datetime, data$Sub_metering_1, type = "l", col = "black")
+  lines(data$datetime, data$Sub_metering_2, type = "l", col = "red")
+  lines(data$datetime, data$Sub_metering_3, type = "l", col = "blue")
+  legend("topright", col = c("black", "red", "blue"), 
+         legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+         lty = c(1,1,1));
+  dev.off()
+  
+}
+plot.subm('required.csv', 'plot3.png')
