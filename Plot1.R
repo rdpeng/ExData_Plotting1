6974e87988393328dbdb4c24e42bfdedec0e023b
Manuel Martínez <mmblanes@drimay.es>
2016-04-11 07:07:46 +0200

    Added files via upload

--- Plot1.R
+++ Plot1.R
@@ -1,11 +1,18 @@
 # @mmblanes #
 #############
 
+## Getting full dataset and subsetting the data
+
 dataFile <- "./household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
+## Converting data
+
 globalActivePower <- as.numeric(subSetData$Global_active_power)
+
+## Creating Plot
+
 png("plot1.png", width=480, height=480)
 hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
