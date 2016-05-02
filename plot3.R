6c17522a2242ef78ecc9a142aeb62d293448b224
ukhf-akt <andre.knuchel-takano@ukhealthforum.org.uk>
2016-05-02 21:27:40 +0100

    Complete

---
+++ plot3.R
@@ -0,0 +1,27 @@
+if(!file.exists("exdata-data-household_power_consumption.zip")) {
+    temp <- tempfile()
+    download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+    file <- unzip(temp)
+    unlink(temp)
+}
+power <- read.table(file, header=T, sep=";")
+power$Date <- as.Date(power$Date, format="%d/%m/%Y")
+df <- power[(power$Date=="2007-02-01") | (power$Date=="2007-02-02"),]
+df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
+df$Global_reactive_power <- as.numeric(as.character(df$Global_reactive_power))
+df$Voltage <- as.numeric(as.character(df$Voltage))
+df <- transform(df, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
+df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
+df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
+
+plot3 <- function() {
+    plot(df$timestamp,df$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+    lines(df$timestamp,df$Sub_metering_2,col="red")
+    lines(df$timestamp,df$Sub_metering_3,col="blue")
+    legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), lwd=c(1,1))
+    dev.copy(png, file="plot3.png", width=480, height=480)
+    dev.off()
+    cat("plot3.png has been saved in", getwd())
+}
+plot3()
