dbe1dffccdcd8443c7153c8b34624325075df083
nikhiltiwari1 <nikhil.tiwari1@gmail.com>
2016-06-06 08:27:41 +0530

    ExDataAssign1
    this assignment includes Rcode for Downloading the data and then
    creating respective plots as desired in this assignment.

---
+++ Plot4.R
@@ -0,0 +1,32 @@
+## Rading the Downlaoded file & Subsetting Data
+
+dataFile <- "./household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+## Culling out Submetering data in the respective variables
+str(subData)
+datetime <- strptime(paste(subData$Date, subData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subData$Global_active_power)
+globalReactivePower <- as.numeric(subData$Global_reactive_power)
+voltage <- as.numeric(subData$Voltage)
+subMetering1 <- as.numeric(subData$Sub_metering_1)
+subMetering2 <- as.numeric(subData$Sub_metering_2)
+subMetering3 <- as.numeric(subData$Sub_metering_3)
+
+## Creating Plot and exporting png
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2, 2)) 
+
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+
+plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
+
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+
+plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
+
+dev.off()
