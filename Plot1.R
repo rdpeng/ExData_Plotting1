c201b81f1c08d04b83bde23696c659235a6475c4
Yiyaoyao <yiyao.zhang1990@gmail.com>
2016-06-09 23:26:31 -0400

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -10,4 +10,4 @@ subdata<-data[data$Date>="2007-02-01" & data$Date<= "2007-02-02",]
 #Plot
 png(filename="plot1.png", width=480, height=480, units="px")
 hist(subdata$Global_active_power,col="red", main= "Global Active Power", xlab= "Global Active Power (kilowatts)")
-dev.off
+dev.off()
