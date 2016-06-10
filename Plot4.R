3039973ee204f252d84f2abdd8bc48d9b72d308e
Yiyaoyao <yiyao.zhang1990@gmail.com>
2016-06-09 23:30:42 -0400

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -1,11 +1,11 @@
 #load the file into R
-data<-read.table("household_power_consumption.txt",header=TRUE,sep=";",na.strings="?")
+data<- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
 
 #Convert date
-data$Date=as.Date(data$Date,format="%d/%m/%Y")
+data$Date<- strptime(data$Date, "%d/%m/%Y")
 
-#select data
-df = data[data$Date==as.Date("2007-02-01")|data$Date==as.Date("2007-02-02"),]
+#subset data
+subdata<-data[data$Date>="2007-02-01" & data$Date<= "2007-02-02",]
 
 #Plot
 png(filename="plot4.png", width=480, height=480, units="px")
