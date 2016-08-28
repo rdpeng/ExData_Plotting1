0f124a8748d03e91b4f83b2e4695a43fe480b4fe
yragab <yusuf.saber@trustious.com>
2016-08-28 07:43:40 +0300

    Adding plot 3 code and output png

---
+++ plot3.R
@@ -0,0 +1,42 @@
+
+# Load packages dplyr and car
+library(car)
+library(dplyr)
+
+# Function to convert string to POSIXct Date
+formatAsDate <- function(date.string) {
+  date = as.POSIXct(as.character(date.string), format = '%d/%m/%Y %H:%M:%S')
+  return(date)
+}
+
+# Read data (with 16GB RAM, I might as well do it) + encode NA
+power.data.raw = read.table("household_power_consumption.txt", header = TRUE, sep = ";", 
+                            stringsAsFactors = FALSE, na.strings = c("?"))
+
+# Subset dataframe %>% Format date
+power.data = power.data.raw %>%
+  filter(Date == "1/2/2007" | Date == "2/2/2007") %>%
+  mutate(DateTime = formatAsDate(paste(Date, Time))) %>%
+  mutate(Global_active_power = as.numeric(Global_active_power))
+
+# Getting superficially famililar with the data
+dim(power.data)
+names(power.data)
+some(power.data)
+
+# Generating plot 3
+with(power.data, {
+  par(bg = NA, mar = c(3,5,1,1)) 
+  plot(Sub_metering_1, ylab = "Energy sub metering", 
+       xaxt = "n", xlab = "", type = "n") 
+  lines(Sub_metering_1, col = "black")
+  lines(Sub_metering_2, col = "red")
+  lines(Sub_metering_3, col = "blue")
+  axis(1, at = nrow(power.data) * c(0,0.5,1), labels = c("Thu", "Fri", "Sat"))
+  legend("topright", col = c("black", "red", "blue"), lwd = 1,
+         legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+})
+
+# Save plot from screen device to png
+dev.copy(png, filename = "plot3.png")
+dev.off()
