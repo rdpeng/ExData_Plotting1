4b3fd385085b1b295e0a9e794f7f8e8fb0ac777b
Wayne81 <cwycuhk2004@yahoo.com.hk>
2016-10-31 21:18:36 +0800

    Read Table Change

--- plot2.R
+++ plot2.R
@@ -1,7 +1,7 @@
 ### Plot 2 ###
 # read data into R
 SrcFile <- "./household_power_consumption.txt"
-SrcData <- read.table(SrcFile, header=TRUE, sep=";", , as.is = TRUE)
+SrcData <- read.table(SrcFile, header=TRUE, sep=";", as.is = TRUE, na.strings = "?")
 
 # Sebset data for analysis
 SrcData["Date"] <- as.Date(SrcData$Date,"%d/%m/%Y")
