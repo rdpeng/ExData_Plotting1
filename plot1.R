5e08e4746bb56683307c539a4e200d245e4310fa
John J Orozco G <john.orozco.giraldo@gmail.com>
2017-03-13 18:00:38 -0500

    Upload 8 files with R code and PNG

---
+++ plot1.R
@@ -0,0 +1,13 @@
+file="./household_power_consumption.txt"
+powerc<-read.csv(file,sep=";",header=TRUE)
+powcfeb2<-powerc[as.Date(powerc$Date,"%d/%m/%Y")==as.Date("02/02/2007","%d/%m/%Y"),]
+powcfeb1<-powerc[as.Date(powerc$Date,"%d/%m/%Y")==as.Date("01/02/2007","%d/%m/%Y"),]
+powcfeb<-rbind(powcfeb1,powcfeb2) 
+#2880rows 9cols
+
+
+powcfeb$Global_active_power<-as.character(p1$Global_active_power)
+png(filename="plot1.png", width=480, height=480,units="px")
+
+hist(as.double(powcfeb$Global_active_power),col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+dev.off()
