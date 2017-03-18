4ce92db997181e912ff646e264eada4c4d0495ae
StephanieStallworth <SStallworth@daytonlamina.com>
2017-03-18 13:37:44 -0400

    Plot 2

---
+++ plot2.R
@@ -0,0 +1,7 @@
+plot1 <- function() {
+    plot(df$timestamp,df$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+    dev.copy(png, file="plot2.png", width=480, height=480)
+    dev.off()
+    cat("plot2.png has been saved in", getwd())
+}
+plot1()
