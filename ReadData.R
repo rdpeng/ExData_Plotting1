ed9d7e0ddedfd9f372aa422e178c87789812459a
StephanieStallworth <SStallworth@daytonlamina.com>
2017-03-18 13:35:05 -0400

    Read and Clean Dataset

---
+++ ReadData.R
@@ -0,0 +1,25 @@
+#Download and Process Data
+
+#Download File
+if(!file.exists("exdata-data-household_power_consumption.zip")) {
+    temp <- tempfile()
+    download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+    file <- unzip(temp)
+    unlink(temp)
+}
+
+#Read table
+power <- read.table(file, header=T, sep=";")
+
+#Format dates
+power$Date <- as.Date(power$Date, format="%d/%m/%Y")
+
+$Subset data
+df <- power[(power$Date=="2007-02-01") | (power$Date=="2007-02-02"),]
+df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
+df$Global_reactive_power <- as.numeric(as.character(df$Global_reactive_power))
+df$Voltage <- as.numeric(as.character(df$Voltage))
+df <- transform(df, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
+df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
+df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
