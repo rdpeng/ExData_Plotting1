90c57982800306ef137edc1af727974fe705bd7a
StephanieStallworth <SStallworth@daytonlamina.com>
2017-03-18 13:39:15 -0400

    Plot 4

---
+++ plot4.R
@@ -0,0 +1,23 @@
+plot4 <- function() {
+    par(mfrow=c(2,2))
+    
+    ##PLOT 1
+    plot(df$timestamp,df$Global_active_power, type="l", xlab="", ylab="Global Active Power")
+    ##PLOT 2
+    plot(df$timestamp,df$Voltage, type="l", xlab="datetime", ylab="Voltage")
+    
+    ##PLOT 3
+    plot(df$timestamp,df$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+    lines(df$timestamp,df$Sub_metering_2,col="red")
+    lines(df$timestamp,df$Sub_metering_3,col="blue")
+    legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), bty="n", cex=.5) #bty removes the box, cex shrinks the text, spacing added after labels so it renders correctly
+    
+    #PLOT 4
+    plot(df$timestamp,df$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
+    
+    #OUTPUT
+    dev.copy(png, file="plot4.png", width=480, height=480)
+    dev.off()
+    cat("plot4.png has been saved in", getwd())
+}
+plot4()
