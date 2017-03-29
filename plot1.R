d06de13bc1ca51e645254a1b285fc4dd96f47251
Sophia0521 <872412360@qq.com>
2017-03-29 15:43:16 +0800

    plot
    plot

--- plot1.R
+++ plot1.R
@@ -1,12 +1,12 @@
 #epc<-read.table("household_power_consumption.txt",header = TRUE,sep = ";")
-break1<-seq(0,6,0.5)
-hist(
-     as.numeric(epc$Global_active_power),
-     cex.lab=1000,
-    
+break1<- seq(0,6,0.5)
+hist1<- hist(
+     as.numeric(epc$Global_active_power)/1000,
+    freq = 1000,
      col="red",
      main="Global_active_power",
      xlab = "Global Active Power(kilowatts)",
      ylab = "Frequency",
      pin=c(480,480)
      )
+hist1
