0dda3045af485016013d9b6449425e599cdd6e1e
shivamgiri <shivamgiri212@gmail.com>
2017-06-04 19:30:29 +0530

    Add files via upload

---
+++ plot4.R
@@ -0,0 +1,17 @@
+file1 <- read.table("C:/Users/shivam giri/Downloads/household_power_consumption.txt",sep = ";",header = TRUE, stringsAsFactors=FALSE,na.strings = "?")
+sub1<-subset(file1,file1$Date=="1/2/2007"|file1$Date=="2/2/2007")
+datetime <- strptime(paste(sub1$Date, sub1$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+
+plot4<-function(){
+  par(mfrow=c(2,2))
+  plot(datetime, sub1$Global_active_power, type="l", xlab=" ",ylab = "Global Active Power(kilowatts)")
+  plot(datetime,sub1$Voltage,type = "l")
+  plot(datetime, sub1$Sub_metering_1, type="l", xlab=" ",ylab = "Energy sub metering")
+  lines(datetime,sub1$Sub_metering_2,type = "l", col="red")
+  lines(datetime,sub1$Sub_metering_3,type = "l", col="blue")
+  legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lwd = 2.5,lty = 1,col=c("black","red","blue"))
+  plot(datetime,sub1$Global_reactive_power,type="l")
+  dev.copy(png,file="plot4.png",width=480,height=480)
+  dev.off()
+}  
+plot4()
