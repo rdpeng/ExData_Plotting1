3e09a6aece57d6cb611dc0501ed66f93374c81b2
Sheryl Harhsberger <h.sheryl@gmail.com>
2017-07-19 08:25:04 -0700

    Week1coursework

---
+++ plot2.R
@@ -0,0 +1,30 @@
+#unzip the file into the working directory
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+temp <- tempfile()
+download.file(url,temp)
+powerfile <- unzip(temp)
+unlink(temp)
+rm(url, temp)
+
+#read data into R
+library(readr)
+powerdata <- read.table(powerfile[1], header = TRUE, sep = ";", na.strings = "?")
+rm(powerfile)
+
+#filter out only the specified dates
+library(dplyr)
+powerdatasmall <- filter(powerdata, Date == "1/2/2007" | Date == "2/2/2007")
+rm(powerdata)
+
+#create the POSIX object
+library(tidyr)
+powerdatasmall$Date <- as.character(powerdatasmall$Date)
+powerdatasmall$Time <- as.character(powerdatasmall$Time)
+powerdatasmall <- mutate(powerdatasmall, datetime = paste(Date, Time, sep = "/"))
+powerdatasmall$datetime <- strptime(powerdatasmall$datetime, format = "%d/%m/%Y/%H:%M:%S")
+
+#graph in a file
+png(filename = "plot2.png", width = 480, height = 480)
+plot(x = powerdatasmall$datetime, y = powerdatasmall$Global_active_power, type = "l",
+     ylab = "Global Active Power (kilowatts)", xlab = "")
+dev.off()
