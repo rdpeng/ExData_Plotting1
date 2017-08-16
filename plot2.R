b351fe8ac50e71c2088bd5fd09ad7f26efa1e483
alan0713 <30411638+alan0713@users.noreply.github.com>
2017-08-16 23:45:08 +0800

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,10 @@
+x<-read.table("household_power_consumption.txt",header = TRUE,sep = ";")
+a<-x[x[,1]=="2/2/2007"]
+b<-x[x[,1]=="1/2/2007",]
+c<-rbind(b,a)
+c[,3]<-as.numeric(as.character(c[,3]))
+t<-paste(c[,1],c[,2])
+newdt<-strptime(t,'%d/%m/%Y %H:%M:%S')
+plot(newdt,c[,3],lwd=1,xlab="",ylab="Global Active Power(kilowatts)",main="",type="l",lty=1)
+dev.copy(png,file="plot2.png",width=480,height=480)
+dev.off()
