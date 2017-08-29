10564dcfcaa1151c746e6097c8c6a6e15f2bbca9
SpencerFishClark <SpencerFishClark@users.noreply.github.com>
2017-08-28 18:44:47 -0700

    Update plot1.R
    added code

---
+++ plot1.R
@@ -0,0 +1,14 @@
+dataFile <- "C:/Users/suzzie/Desktop/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#plotstruc
+
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+
+png("plot1.png", width=480, height=480)
+
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+
+dev.off()
