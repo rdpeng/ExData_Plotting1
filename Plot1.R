ffcfdd04a8ccbd7be830d55d2f4c279259d8fdbf
gopigithubs <32552066+gopigithubs@users.noreply.github.com>
2017-11-19 14:28:10 -0800

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,7 +1,10 @@
 myDataFile <- "./data/household_power_consumption.txt"
+#Read the Data table
 myData <- read.table(myDataFile,header = TRUE,sep=";", stringsAsFactors=FALSE, na.strings="?", dec=".")
+#Subset the data based on the date 
 mySubsetData <- subset (myData, Date %in% c("1/2/2007", "2/2/2007"))
 mySubsetData$Date <- as.Date(mySubsetData$Date, format="%d/%m/%Y")
+#Plotting using histogram 
 hist(mySubsetData$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="red")
 dev.copy(png, file="plot1.png", height=480, width=600)
 dev.off()
