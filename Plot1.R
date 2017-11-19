0fb2dbc1a50295304481b209bb27648edd514ac3
gopigithubs <32552066+gopigithubs@users.noreply.github.com>
2017-11-19 14:30:09 -0800

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -6,5 +6,6 @@ mySubsetData <- subset (myData, Date %in% c("1/2/2007", "2/2/2007"))
 mySubsetData$Date <- as.Date(mySubsetData$Date, format="%d/%m/%Y")
 #Plotting using histogram 
 hist(mySubsetData$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="red")
+#copy the plot to png format
 dev.copy(png, file="plot1.png", height=480, width=600)
 dev.off()
