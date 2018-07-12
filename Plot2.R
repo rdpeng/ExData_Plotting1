4daae793833459d68c57c085111bc738ce187891
Yashika31 <38529090+Yashika31@users.noreply.github.com>
2018-07-12 08:09:30 -0400

    Add files via upload

---
+++ Plot2.R
@@ -0,0 +1,2 @@
+## PLOT 2 - Creating Plot 2 
+> plot(t$Global_active_power~t$dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
---
+++ Plot4.R
@@ -0,0 +1,15 @@
+ par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+>   with(t, {
++     plot(Global_active_power~dateTime, type="l", 
++          ylab="Global Active Power (kilowatts)", xlab="")
++     plot(Voltage~dateTime, type="l", 
++          ylab="Voltage (volt)", xlab="")
++     plot(Sub_metering_1~dateTime, type="l", 
++          ylab="Global Active Power (kilowatts)", xlab="")
++     lines(Sub_metering_2~dateTime,col='Red')
++     lines(Sub_metering_3~dateTime,col='Blue')
++     legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
++            legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
++     plot(Global_reactive_power~dateTime, type="l", 
++          ylab="Global Rective Power (kilowatts)",xlab="")
++   })
