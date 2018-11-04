d3aa6f08300880bab96b1be3d796a40ccb312e8f
mojito9 <44130617+mojito9@users.noreply.github.com>
2018-11-04 11:22:37 +0900

    Add files via upload

---
+++ plot2.r
@@ -0,0 +1,5 @@
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
