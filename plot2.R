5ef2e26d7325d33bdf0fa94338ab4c8bee8cd6ee
Yumiko Abe-Jones <yabejones@users.noreply.github.com>
2019-01-21 02:06:56 -0800

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,25 @@
+library(data.table)
+df <- fread('household_power_consumption.txt',na.strings = c('','NA','NULL'))
+df <- transform(df,
+                Datetime = as.POSIXct(strptime(paste(Date,Time,sep = " "),"%d/%m/%Y %H:%M:%S")),
+                Global_active_power = as.numeric(Global_active_power),
+                Global_reactive_power = as.numeric(Global_reactive_power),
+                Voltage = as.numeric(Voltage),
+                Global_intensity = as.numeric(Global_intensity),
+                Sub_metering_1 = as.numeric(Sub_metering_1),
+                Sub_metering_2 = as.numeric(Sub_metering_2),
+                Sub_metering_3 = as.numeric(Sub_metering_3))
+
+df <- df[(df$Datetime >= "2007-02-01 00:00:00" 
+          & df$Datetime < "2007-02-03 00:00:00"),]
+
+df$Wkday <- as.factor(weekdays(df$Datetime))
+
+
+png("plot2.png",width = 480,height = 480, units = "px")
+par(mar = c(4,5,2,2))
+plot(x = df$Datetime, y = df$Global_active_power,type = "l",
+     ylab = "Global Active Power (kilowatts)",
+     xaxs = 'r',
+     xlab = '')
+dev.off()
