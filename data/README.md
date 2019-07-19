6216e5ae730c853918a7abc51bb9ff96425b108e
mgietzmann <marcel.gietzmann-sanders@viasat.com>
2019-07-18 20:42:57 -0400

    added zipped data

--- data/README.md
+++ data/README.md
@@ -1,1 +1,1 @@
-The data should be downloaded from https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip and unzipped into this folder. 
+The data was downloaded from https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip. 
--- plot1.R
+++ plot1.R
@@ -1,6 +1,6 @@
 library(readr)
 # grab the data and filter to the days of interest
-data <- read_delim("~/repos/ExData_Plotting1/data/household_power_consumption.txt", ";", na=c("", "NA", "?"))
+data <- read_delim("~/repos/ExData_Plotting1/data/exdata_data_household_power_consumption.zip", ";", na=c("", "NA", "?"))
 data$Date <- as.Date(data$Date, "%d/%m/%Y")
 bounds <- as.Date(c("2007-02-01", "2007-02-02"))
 data <- subset(data, bounds[1] <= Date & Date <= bounds[2])
