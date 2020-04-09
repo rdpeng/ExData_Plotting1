be433f9b7e79d4e4ad6a0d8b9bf85d1c41345750
santoshvenkat <62832133+santoshvenkat@users.noreply.github.com>
2020-04-09 14:45:26 +0200

    Peer-Review - Base Plotting - Submission
    This is the submission towards the peer-review assignment for Base Plotting

---
+++ plot2.R
@@ -0,0 +1,11 @@
+library(dplyr)
+library(chron)
+power <- read.csv(file = "power.txt",sep = ";", stringsAsFactors = F)
+power_date <- power %>% mutate(Date = as.Date(Date, format = "%d/%m/%Y"))
+power_date_target <- power_date %>% filter(Date >= "2007-02-01" & Date <= "2007-02-02")  
+power_date_target$Day <- as.POSIXct(paste(power_date_target$Date, power_date_target$Time))
+power_date_target[3:8] <- sapply(power_date_target[3:8],as.numeric)
+ 
+png(filename = "plot2.png",width = 480, height = 480)
+act_pow <- with(power_date_target, plot(Day,Global_active_power,type = "l"))
+dev.off()
