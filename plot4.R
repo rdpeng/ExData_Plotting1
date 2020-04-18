0fcb2b8da623eb614781be3d2d5bb0a629c78287
RomaniBanerjee <63885028+RomaniBanerjee@users.noreply.github.com>
2020-04-18 14:02:18 +0530

    plot4.R

---
+++ plot4.R
@@ -0,0 +1,35 @@
+# set working directory (change this to fit your needs)
+setwd('~/Source Code/GitHub/Exploratory-Data-Analysis')
+
+# make sure the plots folder exists
+if (!file.exists('plots')) {
+  dir.create('plots')
+}
+
+# load data
+source('scripts/get_and_clean_data.R')
+
+# open device
+png(filename='plots/plot4.png',width=480,height=480,units='px')
+
+# make 4 plots
+par(mfrow=c(2,2))
+
+# plot data on top left (1,1)
+plot(power.consumption$DateTime,power.consumption$GlobalActivePower,ylab='Global Active Power',xlab='',type='l')
+
+# plot data on top right (1,2)
+plot(power.consumption$DateTime,power.consumption$Voltage,xlab='datetime',ylab='Voltage',type='l')
+
+# plot data on bottom left (2,1)
+lncol<-c('black','red','blue')
+lbls<-c('Sub_metering_1','Sub_metering_2','Sub_metering_3')
+plot(power.consumption$DateTime,power.consumption$SubMetering1,type='l',col=lncol[1],xlab='',ylab='Energy sub metering')
+lines(power.consumption$DateTime,power.consumption$SubMetering2,col=lncol[2])
+lines(power.consumption$DateTime,power.consumption$SubMetering3,col=lncol[3])
+
+# plot data on bottom right (2,2)
+plot(power.consumption$DateTime,power.consumption$GlobalReactivePower,xlab='datetime',ylab='Global_reactive_power',type='l')
+
+# close device
+x<-dev.off()
