444b7ccc0b22a72eb8a7aa8a29bc340d1694bff8
NamanGupta0611 <64494385+NamanGupta0611@users.noreply.github.com>
2020-05-07 12:57:01 +0530

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,12 @@
+#Set Working Direcory
+setwd("D:/Coursera/R") #Replace by your working Directory
+
+#Read the file
+power <- read.table("household_power_consumption.txt",sep=";",skip = 1)
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity",
+                  "Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subpower <- subset(power,power$Date=="1/2/2007"|power$Date=="2/2/2007")
+
+#calling the basic plot function
+hist(as.numeric(as.character(subpower$Global_active_power)),col="red",main = "Global Active Power",
+     xlab = "Global Active Power(killowatts)")
