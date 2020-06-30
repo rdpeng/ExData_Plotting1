14f44f045897e6be2e875426c7dd7cfa82bca4c9
janexie-piong <67011218+janexie-piong@users.noreply.github.com>
2020-06-29 22:58:15 -0400

    Add files via upload

---
+++ plot4.R
@@ -0,0 +1,19 @@
+data$datetime <- as.POSIXct(data$datetime)
+par(mfrow = c(2, 2))
+attach(data)
+plot(Global_active_power ~ datetime, type = "l", ylab = "Global Active Power", 
+     xlab = "")
+plot(Voltage ~ datetime, type = "l")
+plot(Sub_metering_1 ~ datetime, type = "l", ylab = "Energy sub metering", 
+     xlab = "")
+lines(Sub_metering_2 ~ datetime, col = "Red")
+lines(Sub_metering_3 ~ datetime, col = "Blue")
+legend("topright", lty = 1, col = c("black", "red", "blue"), 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       bty = "n")
+
+plot(Global_reactive_power ~ datetime, type = "l")
+
+dev.copy(png, file = "plot4.png", height = 480, width = 480)
+dev.off()
+detach(data)
