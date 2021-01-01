6526f44ac91fefaa1e72bafcea18cbf9961d4bf5
Spane-05 <dv398@snu.edu.in>
2021-01-02 01:12:37 +0530

    Delete plot2.R

--- Course Project 1/Course Project 1/plot2.R
+++
@@ -1,3 +0,0 @@
-png(filename = "plot2.png", width = 480, height = 480)
-with(elec_data, plot(Date_Time, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)"))
-dev.off()
