30a65491535358ee807ffd7f1df1b468846b9a98
Spane-05 <dv398@snu.edu.in>
2021-01-02 01:12:18 +0530

    Delete plot1.R

--- Course Project 1/Course Project 1/plot1.R
+++
@@ -1,3 +0,0 @@
-png(filename = "plot1.png", width = 480, height = 480)
-with(elec_data, hist(Global_active_power, col="red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)"))
-dev.off()
