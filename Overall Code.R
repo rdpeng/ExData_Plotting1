9e43d39513b54c6f0373ab35fab730f85eb6b3cf
Patel <vpatel@air.org>
2021-02-23 19:31:09 -0600

    Adding subsetting information

---
+++ Overall Code.R
@@ -0,0 +1,6 @@
+setwd("C:/Users/vpatel/OneDrive - American Institutes for Research in the Behavioral Sciences/Desktop/datasciencecoursera/course 4")
+library(data.table)
+library(dplyr)
+data <- read.table("./Week 1/household_power_consumption.txt", sep = ";", header = TRUE)
+data$Date <- as.Date(data$Date,format = "%d/%m/%Y")
+data_subset <- subset(data, data$Date == "2007-02-01" | data$Date == "2007-02-02")
