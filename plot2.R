2728bbeb3088914e11158b05974317d35562ecc6
mmboe <69401291+mmboe@users.noreply.github.com>
2021-03-07 19:05:03 -0500

    Delete plot2.R

--- plot2.R
+++
@@ -1,8 +0,0 @@
-#plot 2 code
-png(filename = "plot2.png", width = 480, height = 480)
-
-plot(dataset$DateTime, dataset$Global_active_power, type = "l", xlab = '', 
-     ylab = "Global Active Power (kilowatts)")
-
-dev.off()
-
