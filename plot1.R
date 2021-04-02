bf8551089d2318f93d09497a498e9a887cf0a184
NIKHIL-PEDDISETTI <nikhilpeddisetti123@gmail.com>
2021-04-03 00:15:23 +0530

    updating

--- plot1.R
+++ plot1.R
@@ -1,13 +1,13 @@
-data_full <- read.csv("E:/ExData_Plotting1/household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
-data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
-# Subseting the data
-data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-rm(data_full)
-# convert Date 
+total_data <- read.csv("C:/Users/nikhi/Desktop/R/ExData_Plotting1/household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+total_data$Date <- as.Date(total_data$Date, format="%d/%m/%Y")
+# Subset the data
+data <- subset(total_data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(total_data)
+# Date conversion
 datetime <- paste(as.Date(data$Date), data$Time)
 data$Datetime <- as.POSIXct(datetime)
 # Plot 1
 hist(data$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
-# Save to file
+# Saving to file
 dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()
--- plot2.R
+++ plot2.R
@@ -1,8 +1,8 @@
-data_full <- read.csv("C:/Users/nikhi/Desktop/R/ExData_Plotting1/household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
-data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
-# Subseting data
-data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-rm(data_full)
+total_data <- read.csv("C:/Users/nikhi/Desktop/R/ExData_Plotting1/household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+total_data$Date <- as.Date(total_data$Date, format="%d/%m/%Y")
+# Subsetting data
+data <- subset(total_data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(total_data)
 #converting Date 
 datetime <- paste(as.Date(data$Date), data$Time)
 data$Datetime <- as.POSIXct(datetime)
