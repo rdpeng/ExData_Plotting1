0579bb435c6f22795af13c6d51778f166569e88d
NIKHIL-PEDDISETTI <nikhilpeddisetti123@gmail.com>
2021-04-02 23:57:45 +0530

    uploading plot4

--- plot4.R
+++ plot4.R
@@ -1,9 +1,9 @@
 data_full <- read.csv("C:/Users/nikhi/Desktop/R/ExData_Plotting1/household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
-# Subsetting data
+# Subsetting the data
 data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 rm(data_full)
-#converting Date 
+# converting Date 
 datetime <- paste(as.Date(data$Date), data$Time)
 data$Datetime <- as.POSIXct(datetime)
 # Plot 4
