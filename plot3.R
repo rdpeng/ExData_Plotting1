fa78c437b8157b74591a7a474d6e70cc90377e2a
Joa3aquin50 <82985528+Joa3aquin50@users.noreply.github.com>
2021-06-01 22:49:23 -0500

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,19 @@
+#read table into R and filter out unneeded dates
+hhpc <- read.table("./household_power_consumption.txt", header = T, 
+                   sep = ";", na.strings = "?")
+hhpc <- hhpc[hhpc$Date == "1/2/2007" | hhpc$Date == "2/2/2007",]
+#convert to POSIXCT
+datetime <- paste(hhpc$Date, hhpc$Time)
+DateTime <- strptime(datetime, format = "%d/%m/%Y %H:%M:%S")
+hhpc <- cbind('DateTime' = DateTime, hhpc)
+#create plot
+png(filename = "plot3.png")
+plot(hhpc$DateTime, hhpc$Sub_metering_1,
+     xlab = "", 
+     ylab = "Energy sub metering", type = "n")
+lines(hhpc$DateTime, hhpc$Sub_metering_1, col = "black", lty = 1, lwd = 1)
+lines(hhpc$DateTime, hhpc$Sub_metering_2, col = "red", lty = 1, lwd = 1)
+lines(hhpc$DateTime, hhpc$Sub_metering_3, col = "blue", lty = 1, lwd = 1)
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       col = c("black", "red", "blue"), lty = 1, lwd = 1)
+dev.off() 
