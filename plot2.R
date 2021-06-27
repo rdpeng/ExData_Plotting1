4e765d34875fa9c3f541e45b22fea314b8d0fdeb
Butterachel <84652431+Butterachel@users.noreply.github.com>
2021-06-27 11:25:32 +0800

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,8 @@
+data <- read.table(file.choose(), sep=";", header=TRUE)
+data[, dateTime = as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+data$Date <- as.Date(data$Date, format = '%d/%m/%Y')
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time))
+if(!file.exists('figures')) dir.create('figures')
+png(filename = './figures/plot2.png', width = 480, height = 480, units='px')
+plot(data$DateTime, data$Global_active_power, xlab = '', ylab = 'Global Active Power (kilowatt)', type = 'l')
+
