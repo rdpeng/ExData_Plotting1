a61ce6bb37517d402899258bfb9df6c68c041a8f
Rajneesh Bansal <rajneeshb231@gmail.com>
2023-12-03 18:44:07 +0530

    sadf

--- plot1.R
+++
@@ -1,9 +0,0 @@
-library(lubridate)
-library(dplyr)
-path<-"C:\\Users\\rajne\\OneDrive\\Desktop\\DataScience-specialisation\\Course-4(Exploratory Data Analysis)\\Project-1\\household_power_consumption.txt"
-df<-read.table(path,sep=";",header = TRUE)
-df$Date<- as.Date(dmy(df$Date))
-df<-subset(df,Date>="2007-02-01" & Date<="2007-02-02")
-png("plot1.png",width = 480,height = 480)
-hist(as.numeric(df$Global_active_power),col="red",xlab = "Global Active Power(killowatts)",ylab = "Frequancy",main = "Global Active Power")
-dev.off()
